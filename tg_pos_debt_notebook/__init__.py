28f11b2468b45bc6c904415f4433c2dd9c86d2c8
Ivan Yelizariev <ivann.exe@gmail.com>
2014-06-03 20:22:30 +0600

    rm print some logs

--- tg_pos_debt_notebook/__init__.py
+++ tg_pos_debt_notebook/__init__.py
@@ -1,4 +1,2 @@
 from openerp.addons.point_of_sale.controllers import main as pos_controller
 pos_controller.html_template = pos_controller.html_template.replace('/keyboard.css" />', '/keyboard.css" />\n<link rel="stylesheet" href="/tg_pos_debt_notebook/static/src/css/tg_pos.css" />')
-print 'html_template', pos_controller.html_template
- 
