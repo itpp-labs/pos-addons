5f5ec0a9f1909858de0c0e978f0144b9297c9762
Ivan Yelizariev <ivann.exe@gmail.com>
2014-07-16 15:55:42 +0600

    upload pos_discount

---
+++ pos_discount/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name' : 'Discount for total amount of pos order',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Point Of Sale',
+    'website' : 'https://it-projects.info',
+    'description': """
+    """,
+    'depends' : ['point_of_sale'],
+    'data':[
+        'data.xml',
+        ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'installable': True,
+    'auto_install': True
+}
