4aa9d6030a0421ce9ec5253a2444fc029f10dabe
Ivan Yelizariev <ivann.exe@gmail.com>
2014-07-24 13:01:05 +0600

    upload pos_product_available

---
+++ pos_product_available/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name' : 'Product available quantity in POS',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category': 'Point Of Sale',
+    'website' : 'https://it-projects.info',
+
+    'depends' : ['point_of_sale', 'stock'],
+    'data':[
+        'data.xml',
+        ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'js': [
+        'static/src/js/pos.js'
+    ],
+    'installable': True,
+    'description': """
+    """,
+}
