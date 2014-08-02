c1008aaeac9ad9803b886d518bbb8cedd39bf5d8
Ivan Yelizariev <ivann.exe@gmail.com>
2014-08-02 16:20:58 +0600

    upload product brand

---
+++ product_brand/product.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import math
+import re
+
+
+from openerp import tools
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+
+import openerp.addons.decimal_precision as dp
+
+
+class product_template(osv.osv):    
+    _inherit = "product.template"
+    _columns = {
+        'branch_id': fields.many2one('product.branch','Product Branch'),
+    }
+
+class product_branch(osv.osv):
+    _name = 'product.branch'
+    _columns = {
+        'name': fields.char('Brand Name',size=250),
+        'description': fields.text('Description'),
+        'product_ids': fields.one2many('product.template','branch_id','Products'),
+    }
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
