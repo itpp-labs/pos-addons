cd88fbaae9e39a8ea2d0d737bec0c95efd357563
Ivan Yelizariev <ivann.exe@gmail.com>
2014-08-04 15:52:58 +0600

    upload product_lot

---
+++ product_lot/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Product lot',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Point Of Sale',
+    'website' : 'https://it-projects.info',
+    'description': """
+Modules allows to create product which is a set of some product (e.g. box of 100 units of same product). Such product can be splitted
+    """,
+    'depends' : ['product', 'stock'],
+    'data':[
+        'views.xml',
+        ],
+    'qweb': [
+        'static/src/xml/pos.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ product_lot/models.py
@@ -0,0 +1,64 @@
+from openerp.osv import fields, orm
+
+class product_product(orm.Model):
+    _inherit = 'product.product'
+
+    def _get_lot_id(self, cr, uid, ids, name, arg, context=None):
+        res = {}
+        for id in ids:
+            lot_id = self.search(cr, uid, [('lot_product_id', '=', id)])
+            res[id] = lot_id and lot_id[0] or None
+        return res
+
+    _columns = {
+        # lot product fields
+        'is_lot': fields.boolean('Lot of products'),
+        'lot_qty': fields.integer('Quantity products in Lot'),
+        'lot_product_id': fields.many2one('product.product', 'Product in lot'), # In fact is one2one
+
+        # normal product fields
+        'lot_id': fields.function(_get_lot_id, type='many2one', relation='product.product', string='Used in Lot')
+    }
+    _defaults = {
+        'is_lot': False
+    }
+
+    def button_split_lot(self, cr, uid, ids, context=None):
+        return self._split_lot(cr, uid, ids[0], context=context)
+
+    def split_lot(self, cr, uid, lot_id, qty, context=None):
+        return self._split_lot(cr, uid, lot_id, qty, context=context)
+
+    def _split_lot(self, cr, uid, lot_id, qty=1.0, context=None):
+        print 'split_lot', lot_id, qty
+        lot = self.browse(cr, uid, lot_id, context=context)
+        assert lot.is_lot, "You can split only lot product"
+        assert lot.lot_product_id, "Product in lot is not specified"
+        assert lot.lot_qty > 0, "Check quantity roducts in lot"
+        stock_move_obj = self.pool.get('stock.move')
+
+        source_location_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'stock', 'stock_location_stock')[1]
+        destination_location_id = source_location_id
+        middle_location_id = lot.property_stock_production.id
+
+        cons_move_id = stock_move_obj.create(cr, uid, {
+            'name': 'split product (consume)',
+            'product_id': lot.id,
+            'product_uom_qty': qty,
+            'product_uom': lot.uom_id.id,
+            'location_id': source_location_id,
+            'location_dest_id': middle_location_id,
+            'company_id': lot.company_id.id,
+        }, context=context)
+
+        prod_move_id = stock_move_obj.create(cr, uid, {
+            'name': 'split product (produce)',
+            'product_id': lot.lot_product_id.id,
+            'product_uom_qty': qty * lot.lot_qty,
+            'product_uom': lot.lot_product_id.uom_id.id,
+            'location_id': middle_location_id,
+            'location_dest_id': destination_location_id,
+            'company_id': lot.lot_product_id.company_id.id,
+        }, context=context)
+        stock_move_obj.action_done(cr, uid, [cons_move_id, prod_move_id], context=context)
+        return True
