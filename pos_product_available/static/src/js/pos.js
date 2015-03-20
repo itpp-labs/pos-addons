bcd7ea707993f82348317709b4525b67c1d0038b
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-20 18:53:46 +0200

    [FIX] check order variable before proceeding

--- pos_product_available/static/src/js/pos.js
+++ pos_product_available/static/src/js/pos.js
@@ -32,11 +32,13 @@ function pos_product_available(instance, module){
         push_order: function(order){
             var self = this;
             var pushed = PosModelSuper.prototype.push_order.call(this, order);
-            order.get('orderLines').each(function(line){
-                var product = line.get_product();
-                product.qty_available -= line.get_quantity();
-                self.refresh_qty_available(product);
-            })
+            if (order){
+                order.get('orderLines').each(function(line){
+                    var product = line.get_product();
+                    product.qty_available -= line.get_quantity();
+                    self.refresh_qty_available(product);
+                })
+            }
             return pushed;
         }
     })
