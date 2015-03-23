3c7185d5e56b981be5ceff6243fdaaafbdf540d0
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-23 11:16:27 +0200

    [IMP] check is customer selected in debt payment
    [FIX] creating newPaymentline
    [FIX] set "cash" type for debt journal

--- tg_pos_debt_notebook/static/src/js/tg_pos.js
+++ tg_pos_debt_notebook/static/src/js/tg_pos.js
@@ -1,5 +1,6 @@
 function tg_pos_debt_notebook(instance, module){ //module is instance.point_of_sale
     //var module = instance.point_of_sale;
+    var _t = instance.web._t;
 
     var PosModelSuper = module.PosModel
     module.PosModel = module.PosModel.extend({
@@ -22,8 +23,8 @@ function tg_pos_debt_notebook(instance, module){ //module is instance.point_of_s
         push_order: function(order){
             var self = this;
             var pushed = PosModelSuper.prototype.push_order.call(this, order);
-            var client = order.get_client();
-            if (client && order){
+            var client = order && order.get_client();
+            if (client){
                 order.get('paymentLines').each(function(line){
                     var journal = line.cashregister.journal;
                     if (!journal.debt)
@@ -45,13 +46,12 @@ function tg_pos_debt_notebook(instance, module){ //module is instance.point_of_s
                     var ss = self.pos.pos_widget.screen_selector;
                     ss.set_current_screen('clientlist');
                 }, 30);
-                return;
             }
 
             var paymentLines = this.get('paymentLines');
-            var newPaymentline = new module.Paymentline({},{cashregister:cashregister});
+            var newPaymentline = new module.Paymentline({},{cashregister:cashregister, pos:this.pos});
 
-            if(journal.type !== 'cash'){
+            if(journal.type !== 'cash' || journal.debt){
                 var val;
                 if (journal.debt)
                     val = -this.getChange() || 0
@@ -63,6 +63,38 @@ function tg_pos_debt_notebook(instance, module){ //module is instance.point_of_s
             this.selectPaymentline(newPaymentline);
         }
 
+    })
+    module.PaymentScreenWidget.include({
+        validate_order: function(options) {
+            var currentOrder = this.pos.get('selectedOrder');
+
+            var isDebt = false;
+            var plines = currentOrder.get('paymentLines').models;
+            for (var i = 0; i < plines.length; i++) {
+                if (plines[i].cashregister.journal.debt) {
+                    isDebt = true;
+                    break;
+                }
+            }
+
+            if (isDebt && !currentOrder.get_client()){
+                this.pos_widget.screen_selector.show_popup('error',{
+                    'message': _t('Unknown customer'),
+                    'comment': _t('You cannot use Debt payment. Select customer first.'),
+                });
+                return;
+            }
+
+            if(isDebt && currentOrder.get('orderLines').models.length === 0){
+                this.pos_widget.screen_selector.show_popup('error',{
+                    'message': _t('Empty Order'),
+                    'comment': _t('There must be at least one product in your order before it can be validated. (Hint: you can use some dummy zero price product)'),
+                });
+                return;
+            }
+
+            this._super(options);
+        }
     })
 }
 
