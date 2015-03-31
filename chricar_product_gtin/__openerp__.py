adcf7b2f4134cbe6a22b64172a9ba272c014f666
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-31 14:32:28 +0200

    * add point_of_sale to dependencies
    * add myself to authors

--- chricar_product_gtin/__openerp__.py
+++ chricar_product_gtin/__openerp__.py
@@ -1,9 +1,9 @@
 { "name"         : "Product GTIN EAN8 EAN13 UPC JPC Support"
 , "version"      : "1.1"
-, "author"       : "Camptocamp"
+, "author"       : "Camptocamp, Ivan Yelizariev"
 , "website"      : "http://www.camptocamp.com"
 , "category"     : "Generic Modules/Others"
-, "depends"      : ["product"]
+, "depends"      : ["product", "point_of_sale"]
 , "description"  : """
 Replaces the EAN13 code completion with a checkroutine for EAN13, EAN8, JPC, UPC and GTIN
 makes EAN visible in simplified view
