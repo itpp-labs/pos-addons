9e613bc6e161101f4a2f26e2a71e32433af808c6
Ilnur Gallyamov <gallyamov@it-projects.info>
2015-05-19 21:51:50 +0500

    description displaced in the README file

--- pos_keyboard/README.rst
+++ pos_keyboard/README.rst
@@ -1,1 +1,16 @@
 USB keyboard in Point of Sale.
+Module to connect an additional usb keyboard for Point of Sale.
+---------------------------------------------------------------
+The processed keys:
+number: 0-9
+mode: 
+ qty: '/'* or "q"**
+ disc: '-'* or 'd'**
+ price: '*'* or 'p'**
+ dot: '.'
+backspace: 'backspace'
++/-: '+'*  or 's'**
+----------------------------
+* key on additional keyboard
+** key on basic keyboard
+
--- pos_keyboard/__openerp__.py
+++ pos_keyboard/__openerp__.py
@@ -2,12 +2,6 @@
 {
     'name': "pos_keyboard",
     'summary': "usb keyboard in point of sale",
-    'description': """
-Module to connect an additional usb keyboard for Point of Sale.
---------------------------------------------------------------
-The processed keys:\n number: 0-9\n mode: qty: "/" disc: "-" price: "*"\n dot: "."\n backspace: 'backspace'\n
-
-""",
     'author': "Ivan Yelizariev",
     "website" : "https://yelizariev.github.io",
     'category' : 'Point Of Sale',
