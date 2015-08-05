3bce99cbea54d4b2fea7c37073c16ab06c08edec
Ivan Yelizariev <yelizariev@it-projects.info>
2015-08-05 12:45:26 +0500

    update field's label

--- pos_multi_session/pos_multi_session_models.py
+++ pos_multi_session/pos_multi_session_models.py
@@ -5,7 +5,7 @@ from openerp import api, models, fields, SUPERUSER_ID
 class pos_config(models.Model):
     _inherit = 'pos.config'
 
-    multi_session_id = fields.Many2one('pos.multi_session', 'Multi session', help='Set the same value for POSes where orders should be synced. Keep empty if this POS should not use syncing')
+    multi_session_id = fields.Many2one('pos.multi_session', 'Multi-session', help='Set the same value for POSes where orders should be synced. Keep empty if this POS should not use syncing')
 
 
 class pos_multi_session(models.Model):
