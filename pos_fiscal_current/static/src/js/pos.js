e7571f970f9113be711caf4d06da844de9877989
Ilyas <rakhimkulov@it-projects.info>
2015-11-26 11:57:09 +0500

    Deleted useless vars

--- pos_fiscal_current/static/src/js/pos.js
+++ pos_fiscal_current/static/src/js/pos.js
@@ -1,16 +1,6 @@
 odoo.define('pos_fiscal_current', function (require) {
 
-    var PosBaseWidget = require('point_of_sale.BaseWidget');
-    var chrome = require('point_of_sale.chrome');
-    var gui = require('point_of_sale.gui');
-    var models = require('point_of_sale.models');
     var screens = require('point_of_sale.screens');
-    var core = require('web.core');
-    var Model = require('web.DataModel');
-    var utils = require('web.utils');
-    var formats = require('web.formats');
-    var QWeb = core.qweb;
-    var _t = core._t;
 
     screens.OrderWidget.include({
         update_summary: function () {
