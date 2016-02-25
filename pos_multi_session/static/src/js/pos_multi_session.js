2711f62d415423b47b0d23da8b5fb447b75960c9
Ilyas <rakhimkulov@it-projects.info>
2016-02-25 16:24:58 +0500

    I have added demo2 user. Also i have removed piece of code intended for 9.0 that breaks 8.0

--- pos_multi_session/static/src/js/pos_multi_session.js
+++ pos_multi_session/static/src/js/pos_multi_session.js
@@ -201,19 +201,8 @@ openerp.pos_multi_session = function(instance){
                 order.get('orderLines').remove(line);
             })
 
-        },
-        load_server_data: function(){
-            res = PosModelSuper.prototype.load_server_data.apply(this, arguments);
-            var self = this;
-            return res.then(function(){
-                             if (self.config.multi_session_id){
-                                 self.multi_session = new exports.MultiSession(self);
-                                 self.multi_session.start();
-                                 self.multi_session.request_sync_all();
-                             }
-                         })
-        },
-    })
+        }
+    });
 
     var is_first_order = true;
     var OrderSuper = module.Order;
