5ec1cad61b14ab0f7a99fe7e160641d1e276a1a0
ufaks <excellm@yandex.ru>
2016-03-22 12:17:09 +0500

    temp_commit #61

--- pos_debt_notebook/__openerp__.py
+++ pos_debt_notebook/__openerp__.py
@@ -8,7 +8,7 @@
     'website': 'https://it-projects.info',
     'depends': ['point_of_sale', 'account'],
     'demo': [
-        'test/account_minimal_test.xml',
+        # 'test/account_minimal_test.xml',
         ],
     'data': [
         'data.xml',
