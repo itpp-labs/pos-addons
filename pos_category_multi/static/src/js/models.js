1f0688fbbb42c0c18b3aa3ae3a4bf74bd8f4e980
x620 <romanchenko@it-projects.info>
2016-04-22 18:39:58 +0500

    [FIX] remove unused code [IMP] added widget="many2many_tags" and help for field

--- pos_category_multi/static/src/js/models.js
+++ pos_category_multi/static/src/js/models.js
@@ -1,17 +1,7 @@
 odoo.define('pos_category_multi.models', function (require) {
     "use strict";
 
-    var PosDB = require('pos_category_multi.DB');
     var exports = require('point_of_sale.models');
-
-    // overrides this.db in exports.PosModel
-    var _super_posmodel = exports.PosModel.prototype;
-    exports.PosModel = exports.PosModel.extend({
-        initialize: function (session, attributes) {
-            _super_posmodel.initialize.apply(this, arguments);
-            this.db = new PosDB();
-        }
-    });
 
     // overrides models in exports.PosModel
     var models = exports.PosModel.prototype.models;
