19cb2d03c183fe87ac2f456f3c6be7dca037f792
ilmir <ilmir.karamov@gmail.com>
2016-04-27 21:39:12 +0500

    [DOC] Description for the pos_disable_payment module

--- pos_disable_payment/__openerp__.py
+++ pos_disable_payment/__openerp__.py
@@ -1,10 +1,12 @@
 {
     'name': "Disable payments in POS",
+    'summary': "Control access to the POS payments",
     'version': '2.1.1',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
     'category': 'Point Of Sale',
     'website': 'https://yelizariev.github.io',
+    'images': ['images/pos_payment_access.png'],
     'depends': ['point_of_sale','base'],
     'data': [
         'views.xml',
