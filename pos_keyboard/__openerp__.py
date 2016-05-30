1d4eaf0b2ff68cdbb4133b83316d2253c9543a5f
Thomas Rehn <thomas.rehn@initos.com>
2016-05-30 10:50:07 +0200

    [ADD] backport module `pos_keyboard` from v8 to v7

---
+++ pos_keyboard/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Keyboard support in Point Of Sale",
+    'author': "IT-Projects LLC, Ivan Yelizariev",
+    'summary': 'Module allows to use usual keyboard (not virtual one) in Point of Sale',
+    "website": "https://it-projects.info",
+    'images': ['images/keyboard.png'],
+    'category': 'Point Of Sale',
+    'version': '7.0.1.0.0',
+    'depends': ['point_of_sale'],
+    'js': [
+         'static/src/js/pos.js',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'description': """
+Keyboard support in Point Of Sale
+================================================================
+Module allows to use usual keyboard (not virtual one) in Point of Sale.
+
+Usage:
+------
+Using keys below switch to mode you need. Qty mode is used by default.
+Then use number keys to enter quantity, price or discount.
+
+
+=========== ===================== =================
+Type        Numpad                Extra keys  
+=========== ===================== =================
+mode qty    ``/``                 ``q``
+----------- --------------------- -----------------
+mode disc   ``-``                 ``d``
+----------- --------------------- -----------------
+mode price  ``*``                 ``p``
+----------- --------------------- -----------------
++/-         ``+``                 ``s``
+=========== ===================== =================
+""",
+}
---
+++ pos_keyboard/doc/changelog.rst
@@ -0,0 +1,9 @@
+.. _changelog:
+
+Changelog
+=========
+
+`7.0.1.0.0`
+-------
+
+- Backport of 1.0.2 for Odoo v8 to Odoo v7
