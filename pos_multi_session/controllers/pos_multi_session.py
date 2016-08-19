157dab55a151996a1e0b7a8f35e1296e676a5e87
Ivan Yelizariev <yelizariev@it-projects.info>
2016-08-19 17:29:11 +0500

    [REF] fix travis errors:
    * update way of import bus controller

--- pos_multi_session/controllers/pos_multi_session.py
+++ pos_multi_session/controllers/pos_multi_session.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import openerp
 from openerp.http import request
+from openerp.addons.bus.bus import Controller as bus_controller
 
 
-class Controller(openerp.addons.bus.bus.Controller):
+class Controller(bus_controller):
 
     def _poll(self, dbname, channels, last, options):
         if request.session.uid:
