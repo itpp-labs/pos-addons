8d29810418002206fdad8e89262ec91663de6ef6
manawi <manaewich@gmail.com>
2016-10-11 09:35:44 +0500

    [FIX] flake8 E128

--- pos_category_multi/models.py
+++ pos_category_multi/models.py
@@ -6,5 +6,4 @@ from openerp import fields, models
 class PosCategoryMulti(models.Model):
     _inherit = 'product.template'
 
-    pos_category_ids = fields.Many2many('pos.category', string='Point of Sale Categories',
-        help='Those categories are used to group similar products for point of sale.')
+    pos_category_ids = fields.Many2many('pos.category', string='Point of Sale Categories', help='Those categories are used to group similar products for point of sale.')
