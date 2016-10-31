737559bb6fe2c2f9c5cc63e6b6cec6817b6b87f9
Dinar <gabbasov@it-projects.info>
2016-10-31 14:11:06 +0500

    [IMP] pos_multi_session: save sync data on server  (#153)

--- pos_multi_session/external_tests/tests/test_sync.py
+++ pos_multi_session/external_tests/tests/test_sync.py
@@ -41,6 +41,20 @@ class TestSync(TestCommon):
             "admin": {},
             "demo": {}
         }, [
+            # admin removes orders
+            {"session": "admin",
+             "code": """
+                 mstest.remove_all_orders();
+             """,
+             },
+            # demo removes orders
+            {"session": "demo",
+             "code": """
+                 mstest.wait(function(){
+                    mstest.remove_all_orders();
+                 })
+             """,
+             },
             # admin creates order
             {"session": "admin",
              "code": """
@@ -65,26 +79,46 @@ class TestSync(TestCommon):
              "code": """
                  mstest.fill_order();
                  mstest.wait(function(){
-                     mstest.fill_order();
-                 })
+                    mstest.fill_order();
+                    // print current state of the order
+                    mstest.save_order();
+                 }, 3000)
              """,
              },
             # GC
             {"session": "admin",
              "code": """
-                 share.order = mstest.save_order();
                  mstest.wait(function(){
                      mstest.gc();
                  }, 2000);
              """,
              },
+            # demo creates new order
+            {"session": "demo",
+             "code": """
+                mstest.new_order();
+                mstest.wait(function(){
+                    mstest.new_order();
+                }, 6000);
+             """,
+             },
             # demo is on
             {"session": "demo",
              "extra": "connection_on",
+             },
+            # admin updates order
+            {"session": "admin",
              "code": """
-                 mstest.wait(function(){
-                     mstest.find_order(share.order);
-                 }, 3000)
+                 mstest.fill_order();
+                 share.order = mstest.save_order();
+             """,
+             },
+            # check sync on demo
+            {"session": "demo",
+             "code": """
+             mstest.wait(function(){
+                mstest.find_order(share.order);
+            }, 9000)
              """,
              },
             # ok
--- pos_multi_session/pos_multi_session_models.py
+++ pos_multi_session/pos_multi_session_models.py
@@ -2,6 +2,7 @@
 from openerp import api
 from openerp import fields
 from openerp import models
+import json
 
 
 class PosConfig(models.Model):
@@ -11,6 +12,7 @@ class PosConfig(models.Model):
     multi_session_accept_incoming_orders = fields.Boolean('Accept incoming orders', default=True)
     multi_session_replace_empty_order = fields.Boolean('Replace empty order', default=True, help='Empty order is deleted whenever new order is come from another POS')
     multi_session_deactivate_empty_order = fields.Boolean('Deactivate empty order', default=False, help='POS is switched to new foreign Order, if current order is empty')
+    multi_session_message_ID = fields.Integer(default=1, string="Last sent message number")
 
 
 class PosMultiSession(models.Model):
@@ -18,12 +20,97 @@ class PosMultiSession(models.Model):
 
     name = fields.Char('Name')
     pos_ids = fields.One2many('pos.config', 'multi_session_id', 'POSes')
+    order_ID = fields.Integer(string="Order number", default=1, help="Current Order Number shared across all POS in Multi Session")
+    order_ids = fields.One2many('pos.multi_session.order', 'multi_session_id')
 
-    @api.one
-    def broadcast(self, message):
+    @api.multi
+    def on_update_message(self, message):
+        self.ensure_one()
+        if message['action'] == 'update_order':
+            res = self.set_order(message)
+        elif message['action'] == 'sync_all':
+            res = self.get_sync_all(message)
+        elif message['action'] == 'remove_order':
+            res = self.remove_order(message)
+        else:
+            res = self.broadcast_message(message)
+        return res
+
+    @api.multi
+    def set_order(self, message):
+        self.ensure_one()
+        msg_data = message['data']
+        order_uid = msg_data['uid']
+        order = self.env['pos.multi_session.order'].search([('order_uid', '=', order_uid)])
+        if order:  # order already exists
+            order.write({
+                'order': json.dumps(message),
+            })
+        else:
+            order.create({
+                'order': json.dumps(message),
+                'order_uid': order_uid,
+                'multi_session_id': self.id,
+            })
+        self.broadcast_message(message)
+        return 1
+
+    @api.multi
+    def get_sync_all(self, message):
+        self.ensure_one()
+        pos_id = message['data']['pos_id']
+        pos = self.env['pos.config'].search([('multi_session_id', '=', self.id), ("id", "=", pos_id)])
+        message = []
+        for order in self.order_ids:
+            msg = json.loads(order.order)
+            msg['data']['message_ID'] = pos.multi_session_message_ID
+            msg['action'] = 'sync_all'
+            message.append(msg)
+        return message
+
+    @api.multi
+    def remove_order(self, message):
+        self.ensure_one()
+        msg_data = message['data']
+        order_uid = msg_data['uid']
+        self.order_ids.search([('order_uid', '=', order_uid)]).unlink()
+        self.broadcast_message(message)
+        return 1
+
+    @api.multi
+    def broadcast_message(self, message):
+        self.ensure_one()
+        pos_id = message['data']['pos_id']
+        for r in self.env['pos.config'].search([('id', '!=', pos_id), ('multi_session_id', '=', self.id)]):
+            r.write({
+                'multi_session_message_ID': r.multi_session_message_ID + 1
+            })
         notifications = []
         for ps in self.env['pos.session'].search([('state', '!=', 'closed'), ('config_id.multi_session_id', '=', self.id)]):
             if ps.user_id.id != self.env.user.id:
+                message_ID = self.env['pos.config'].search([('id', '=', ps.config_id.id)]).multi_session_message_ID
+                message['data']['message_ID'] = message_ID
                 notifications.append([(self._cr.dbname, 'pos.multi_session', ps.user_id.id), message])
         self.env['bus.bus'].sendmany(notifications)
         return 1
+
+
+class PosMultiSessionOrder(models.Model):
+    _name = 'pos.multi_session.order'
+
+    order = fields.Text('Order JSON format')
+    order_uid = fields.Char()
+    multi_session_id = fields.Many2one('pos.multi_session', 'Multi session')
+
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+
+    @api.multi
+    def wkf_action_closing_control(self):
+        self.config_id.write({'multi_session_message_ID': 1})
+        res = super(PosSession, self).wkf_action_closing_control()
+        active_sessions = self.env['pos.session'].search([('state', '!=', 'closed'), ('config_id.multi_session_id', '=', self.config_id.multi_session_id.id)])
+        if len(active_sessions) == 0:
+            self.config_id.multi_session_id.write({'order_ID': 1})
+        return res
