0b1df01206f523259c7b6f0e6b08082be49b5903
Ivan Yelizariev <yelizariev@it-projects.info>
2016-11-04 15:00:38 +0500

    fix grammar. Put comment before the code

--- pos_multi_session/external_tests/tests/test_sync.py
+++ pos_multi_session/external_tests/tests/test_sync.py
@@ -219,8 +219,8 @@ class TestSync(TestCommon):
              "code": """
                  mstest.print_order();
                  synced_order = mstest.get_order();
+                 /* this test is currently not supported: request from demo is ignored and his state is rollbacked */
                  // mstest.check_inclusion(share.demo_order, synced_order);
-                 // this test is currently not supported: request from demo is ignored and hist state is rollbacked
              """,
              },
             # demo is on
