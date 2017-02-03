41693429c8cd005534829ea52da1d4627b6586e2
Ivan Yelizariev <yelizariev@it-projects.info>
2017-02-03 14:17:41 +0500

    [FIX] pos_longpolling: fix domain to check closed session
    [IMP] add logs
    [REF] _get_full_channel_name in python
    [IMP] add_channel -- allow to pass thisArg

--- pos_longpolling/models/pos_longpolling_models.py
+++ pos_longpolling/models/pos_longpolling_models.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import logging
 from odoo import api, models
+
+_logger = logging.getLogger(__name__)
 
 
 class PosConfig(models.Model):
@@ -8,8 +11,15 @@ class PosConfig(models.Model):
     @api.multi
     def _send_to_channel(self, channel_name, message):
         notifications = []
-        for ps in self.env['pos.session'].search([('state', '!=', 'closed'), ('id', 'in', self.ids)]):
-            channel = '["%s","%s","%s"]' % (self._cr.dbname, channel_name, ps.config_id.id)
+        for ps in self.env['pos.session'].search([('state', '!=', 'closed'), ('config_id', 'in', self.ids)]):
+            channel = ps.config_id._get_full_channel_name(channel_name)
             notifications.append([channel, message])
-        self.env['bus.bus'].sendmany(notifications)
+        if notifications:
+            self.env['bus.bus'].sendmany(notifications)
+        _logger.debug('POS notifications for %s: %s', self.ids, notifications)
         return 1
+
+    @api.multi
+    def _get_full_channel_name(self, channel_name):
+        self.ensure_one()
+        return '["%s","%s","%s"]' % (self._cr.dbname, channel_name, self.id)
