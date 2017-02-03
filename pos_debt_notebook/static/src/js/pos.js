73328df9b22664c48af0ae5e09e7655ebf796fff
GabbasovDinar <gabbasov@it-projects.info>
2017-02-03 12:39:14 +0500

    [ADD] added queue to requests

--- pos_debt_notebook/static/src/js/pos.js
+++ pos_debt_notebook/static/src/js/pos.js
@@ -14,6 +14,8 @@ odoo.define('pos_debt_notebook.pos', function (require) {
     var _super_posmodel = models.PosModel.prototype;
     models.PosModel = models.PosModel.extend({
         initialize: function (session, attributes) {
+            this.partner_list_ids = [];
+            this.reload_debts_ready = $.when();
             var partner_model = _.find(this.models, function(model){ return model.model === 'res.partner'; });
             partner_model.fields.push('debt_type', 'debt', 'debt_limit');
             var journal_model = _.find(this.models, function(model){ return model.model === 'account.journal'; });
@@ -59,9 +61,13 @@ odoo.define('pos_debt_notebook.pos', function (require) {
             // function is called whenever we need to update debt value from server
             var limit = 0; // download only new debt value
             limit = 10; //debug
-            return this._load_debts(partner_ids, limit).then(function(data){
-                //TODO
-                console.log('partner debt', data);
+            var self = this;
+            this.partner_list_ids = this.partner_list_ids.concat(partner_ids);
+            this.reload_debts_ready = this.reload_debts_ready.then(function(){
+                return self._load_debts(self.partner_list_ids, limit).then(function(data){
+                    //TODO
+                    console.log('partner debt', data);
+                });
             });
         },
         _load_debts: function(partner_ids, limit){
