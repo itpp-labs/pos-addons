0cc09c773570d992d1fb3559e0d80acae3127ac7
Ivan Yelizariev <yelizariev@it-projects.info>
2017-02-13 14:50:40 +0500

    [DOC] bump 4.1.0
    ADD: Invoice support
    FIX: Fetch new partners before loading their debt history (e.g. when another POS create partner)
    ADD: print prev and new debt value in receipt as well as customer name

--- pos_debt_notebook/__openerp__.py
+++ pos_debt_notebook/__openerp__.py
@@ -8,7 +8,7 @@
         'images/screenshot-3.png',
         'images/screenshot-1.png',
     ],
-    'version': '4.0.0',
+    'version': '4.1.0',
 
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     "support": "apps@it-projects.info",
