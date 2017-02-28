38cfaad9657bab04651375dedc336a823a1e4080
GabbasovDinar <gabbasov@it-projects.info>
2017-02-28 11:04:38 +0500

    [FIX] Fixes travis errors

--- pos_longpolling/controllers/pos_longpolling_controller.py
+++ pos_longpolling/controllers/pos_longpolling_controller.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-import datetime
 import logging
-import json
 
 import openerp
 from openerp.http import request
 from openerp.addons.bus.bus import Controller as bus_controller
-from openerp.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT
 
 _logger = logging.getLogger(__name__)
 
