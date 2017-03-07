e6397dbe2f1adde446cb1bc899ece875a70c83d6
Dinar <gabbasov@it-projects.info>
2017-03-07 21:25:19 +0500

    [FIX] longpolling channel error [ADD] help message for query_timeout and response_timeout fields - pos_longpolling v9.0 (#252)
    * [FIX] Fixes channel error, update changelog
    
    * [ADD] Add help for timeout fields

--- pos_longpolling/__openerp__.py
+++ pos_longpolling/__openerp__.py
@@ -4,7 +4,7 @@
     "summary": """Technical module implement instant updates in POS""",
     "category": "Point of Sale",
     "images": [],
-    "version": "1.1.0",
+    "version": "1.1.1",
     "application": False,
 
     "author": "IT-Projects LLC, Dinar Gabbasov",
