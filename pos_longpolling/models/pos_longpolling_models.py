4405815fff1ff5abbd802eabcd109ae328fccbf6
GabbasovDinar <gabbasov@it-projects.info>
2017-03-07 15:29:57 +0500

    [ADD] Add help for timeout fields

--- pos_longpolling/models/pos_longpolling_models.py
+++ pos_longpolling/models/pos_longpolling_models.py
@@ -8,8 +8,19 @@ _logger = logging.getLogger(__name__)
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    query_timeout = fields.Float(string='Query timeout', default=0.0833)
-    response_timeout = fields.Float(string='Response timeout', default=0.01666)
+    # 5/60 = 0.0833 = 5 min - default value
+    query_timeout = fields.Float(string='Query timeout', default=0.0833,
+                                 help="Waiting period for any message from poll "
+                                      "(if we have not received a message at this period, "
+                                      "poll will send message ('PING') to check the connection)")
+
+    # 1/60 = 0.01666 = 1 min  - default value
+    response_timeout = fields.Float(string='Response timeout', default=0.01666,
+                                    help="Waiting period for response message (i.e. once message from "
+                                         "poll has been sent, it will be waiting for response message ('PONG') "
+                                         "at this period and if the message has not been received, the icon turns "
+                                         "color to red. Once the connection is restored, the icon changes its color "
+                                         "back to green)")
 
     @api.multi
     def _send_to_channel(self, channel_name, message):
