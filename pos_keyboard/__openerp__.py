5e15d1d9a9a98100ac9fae91b2b66d60ffc7cd78
ilmir <ilmir.karamov@gmail.com>
2017-05-25 21:25:36 +0500

    [DOC] pos_keyboard: new price

--- pos_keyboard/__openerp__.py
+++ pos_keyboard/__openerp__.py
@@ -8,7 +8,7 @@
     'category': 'Point Of Sale',
     'version': '1.0.2',
     'depends': ['point_of_sale'],
-    "price": 100.00,
+    "price": 15.00,
     "currency": "EUR",
     'data': [
         'data.xml',
