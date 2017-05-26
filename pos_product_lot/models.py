555af398977823adaee3f68a1ed83cf0b878fd8f
ArtyomLosev <losev@it-projects.info>
2017-05-26 16:44:30 +0500

    [Port][10.0] pos_product_lot (#301)
    * [Port] 10.0-pos_product_lot

--- pos_product_lot/models.py
+++ pos_product_lot/models.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
+from odoo import models
 
 
-
-class ProductProduct(orm.Model):
+class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    def split_lot_from_ui(self, cr, uid, records, context=None):
+    def split_lot_from_ui(self, records):
         res = []
         for r in records:
-            self._split_lot(cr, uid,
-                            r['product']['id'], r['qty'],
-                            context=context)
-            res.append(r['id'])
+            product_id = r['product']['id']
+            qty = r['qty']
+            self.env['product.product'].search([('id', '=', product_id)])._split_lot(qty)
+            res.append(product_id)
         return res
