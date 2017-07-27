09b963e34bc6e072fee406bf05cb43aacb268fd9
Dinar <gabbasov@it-projects.info>
2017-07-27 16:08:41 +0500

    [10.0][ADD] pos_order_note: the possibility to use several notes simultaneously (it-projects-llc/addons-dev#242)
    * [ADD] Added active notes
    
    * [ADD] Added custom order and orderline note
    
    * [ADD] Added custom notes for 'other' button in popup
    
    * [FIX] Fixes order printer error for custom notes
    
    * [FIX] Fixes error for order printer
    
    * [FIX] custom notes errors for order printer
    
    * [FIX]  fixes calculate default value for field, rename field
    
    * [FIX] print order after cancelled note
    
    * [FIX] requred fields
    
    * [FIX] active the order button error
    
    * [FIX] splitbill error
    
    * [FIX] Fix custom note errors for splitbill button

--- pos_order_note/models/models.py
+++ pos_order_note/models/models.py
@@ -3,12 +3,21 @@ from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    pos_notes = fields.Text('Notes for POS', translate=True)
+    _inherit = "product.template"
+    pos_notes = fields.Text("Notes for POS", translate=True)
 
 
 class PosProductNotes(models.Model):
     _name = "pos.product_notes"
 
-    number = fields.Integer(string="Number")
+    def _default_sequence(self):
+        numbers = self.env["pos.product_notes"].search([])
+        sequences = []
+        for r in numbers:
+            sequences.append(r.number)
+        if sequences:
+            return max(sequences) + 1
+        return 0
+
+    number = fields.Integer(string="Sequence", default=_default_sequence)
     name = fields.Char(string="Note")
