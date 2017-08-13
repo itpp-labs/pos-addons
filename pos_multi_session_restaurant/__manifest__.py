1d42a740d74b100c35db809a8d33d748400b75f1
Ivan Yelizariev <yelizariev@it-projects.info>
2017-08-13 08:26:52 +0500

    [DOC] bump 1.1.6

--- pos_multi_session_restaurant/__manifest__.py
+++ pos_multi_session_restaurant/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": """Staff get order details immediately after waiter taps on tablet""",
     "category": "Point of Sale",
     "images": ['images/s2.png'],
-    "version": "1.1.5",
+    "version": "1.1.6",
     "application": False,
 
     "author": "IT-Projects LLC, Ivan Yelizariev",
