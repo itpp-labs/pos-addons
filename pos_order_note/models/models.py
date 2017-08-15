feb6370a47c7104ef6eb92a9b1054525c1259227
GabbasovDinar <gabbasov@it-projects.info>
2017-08-15 11:18:28 +0500

    [FIX] remove sequence field in form and tree view

--- pos_order_note/models/models.py
+++ pos_order_note/models/models.py
@@ -10,14 +10,5 @@ class ProductTemplate(models.Model):
 class PosProductNotes(models.Model):
     _name = "pos.product_notes"
 
-    def _default_sequence(self):
-        numbers = self.env["pos.product_notes"].search([])
-        sequences = []
-        for r in numbers:
-            sequences.append(r.number)
-        if sequences:
-            return max(sequences) + 1
-        return 0
-
-    number = fields.Integer(string="Sequence", default=_default_sequence)
+    sequence = fields.Integer(string="Sequence")
     name = fields.Char(string="Note")
