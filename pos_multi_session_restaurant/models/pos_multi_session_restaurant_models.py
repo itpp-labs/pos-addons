fd3a248dea36c235c849acd5c30c4a04719b1b4c
Your Name <kolushov@it-projects.info>
2017-09-22 12:12:12 +0500

    [IMP] ondelete is changed from cascade to restrict

--- pos_multi_session_restaurant/models/pos_multi_session_restaurant_models.py
+++ pos_multi_session_restaurant/models/pos_multi_session_restaurant_models.py
@@ -12,10 +12,11 @@ class PosMultiSession(models.Model):
     _inherit = 'pos.multi_session'
 
     floor_ids = fields.Many2many('restaurant.floor', 'pos_multi_session_floor_rel', 'pos_multi_session_id', 'floor_id',
-                                 string='Restaurant Floors', help='The restaurant floors served by this point of sale')
+                                 string='Restaurant Floors', help='The restaurant floors served by this point of sale',
+                                 ondelete="restrict")
 
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    ms_floor_ids = fields.Many2many(related='multi_session_id.floor_ids')
+    ms_floor_ids = fields.Many2many(related='multi_session_id.floor_ids', ondelete="restrict")
