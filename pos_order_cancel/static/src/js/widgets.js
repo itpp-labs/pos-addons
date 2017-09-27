72434f728ad31411b824bf23249d04481fb25d50
gael.torrecillas <gael.torrecillas@mind-and-go.com>
2017-09-27 14:38:27 +0200

    [pos_order_cancel] add option to allow free reason

--- pos_order_cancel/static/src/js/widgets.js
+++ pos_order_cancel/static/src/js/widgets.js
@@ -137,20 +137,24 @@ odoo.define('pos_order_cancel.widgets', function (require) {
             }
         },
         click_confirm: function(){
-            this.gui.close_popup();
-            if( this.options.confirm ){
-                var active_reasons = this.options.reasons.filter(function(item) {
-                    return item.active === true;
-                });
-                var active_reasons_name = [];
-                active_reasons.forEach(function(item) {
-                    active_reasons_name.push(item.name);
-                });
-                var reason = this.$('.popup-confirm-cancellation textarea').val();
-                if (reason) {
-                    reason += "; ";
-                }
-                this.options.confirm.call(this, reason + active_reasons_name.join("; "));
+        	var active_reasons = this.options.reasons.filter(function(item) {
+                return item.active === true;
+            });
+            var active_reasons_name = [];
+            active_reasons.forEach(function(item) {
+                active_reasons_name.push(item.name);
+            });
+            
+            if(this.pos.config.accept_free_reason || active_reasons_name.length>0)
+            {
+                this.gui.close_popup();
+                if( this.options.confirm ){
+                    var reason = this.$('.popup-confirm-cancellation textarea').val();
+                    if (reason) {
+                        reason += "; ";
+                    }
+                    this.options.confirm.call(this, reason + active_reasons_name.join("; "));
+	            }
             }
         },
     });
