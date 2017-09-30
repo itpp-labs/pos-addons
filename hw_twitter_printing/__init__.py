32992074fecbe1343199a818179f47ec3ccfec20
GabbasovDinar <gabbasov@it-projects.info>
2017-09-30 16:03:44 +0500

    [REF] remove old code

--- hw_twitter_printing/__init__.py
+++ hw_twitter_printing/__init__.py
@@ -7,11 +7,9 @@ from odoo.tools import config
 _logger = logging.getLogger(__name__)
 
 
-twython_imported = False
 try:
     from twython import TwythonStreamer
     from escpos.printer import Network
-    twython_imported = True
 except ImportError as err:
     TwythonStreamer = object
     _logger.debug(err)
@@ -26,14 +24,15 @@ class MyStreamerThread(threading.Thread):
     def run(self):
         _logger.info("MyStreamerThread started.")
 
-        app_key = config['app_key']
-        app_secret = config['app_secret']
-        oauth_token = config['oauth_token']
-        oauth_token_secret = config['oauth_token_secret']
-        if twython_imported:
-            stream = MyStreamer(app_key, app_secret, oauth_token, oauth_token_secret)
-            stream.printer = False
-            stream.statuses.filter(track='#OdooExperience,#OdooExperience2017')
+        app_key = config['twitter_app_key']
+        app_secret = config['twitter_app_secret']
+        oauth_token = config['twitter_oauth_token']
+        oauth_token_secret = config['twitter_oauth_token_secret']
+        twitter_search = config['twitter_search']
+
+        stream = MyStreamer(app_key, app_secret, oauth_token, oauth_token_secret)
+        stream.printer = False
+        stream.statuses.filter(track=twitter_search)
 
 
 class MyStreamer(TwythonStreamer):
