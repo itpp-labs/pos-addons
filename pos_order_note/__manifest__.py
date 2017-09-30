7aac85a5184eb9b202ed3e633e5f66cbcc0206c6
GabbasovDinar <gabbasov@it-projects.info>
2017-09-30 13:29:23 +0500

    [REF] refactoring after change pos_restaurant_base

--- pos_order_note/__manifest__.py
+++ pos_order_note/__manifest__.py
@@ -15,7 +15,7 @@
     "currency": "EUR",
 
     "depends": [
-        "pos_restaurant",
+        "pos_restaurant_base",
     ],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
