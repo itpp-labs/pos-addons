c3e8f70da2e24311de9c4ea21540a23ca7d437a5
Aleksandr <kolushov@it-projects.info>
2017-10-14 15:27:00 +0500

    [DEV]  features are moved to pos_multi_session_sync (#429)
    * necessary refactoring made to be allow sync via external odoo
    
    (restored commit)

--- pos_multi_session/models/pos_multi_session_models.py
+++ pos_multi_session/models/pos_multi_session_models.py
@@ -17,7 +17,6 @@ class PosConfig(models.Model):
     multi_session_accept_incoming_orders = fields.Boolean('Accept incoming orders', default=True)
     multi_session_replace_empty_order = fields.Boolean('Replace empty order', default=True, help='Empty order is deleted whenever new order is come from another POS')
     multi_session_deactivate_empty_order = fields.Boolean('Deactivate empty order', default=False, help='POS is switched to new foreign Order, if current order is empty')
-    multi_session_message_ID = fields.Integer(default=1, string="Last sent message number")
     current_session_state = fields.Char(search='_search_current_session_state')
     sync_server = fields.Char(related='multi_session_id.sync_server')
     autostart_longpolling = fields.Boolean(default=False)
@@ -40,6 +39,23 @@ class PosMultiSession(models.Model):
 
     name = fields.Char('Name')
     pos_ids = fields.One2many('pos.config', 'multi_session_id', string='POSes in Multi-session')
-    order_ids = fields.One2many('pos_multi_session_sync.order', 'multi_session_ID', 'Orders')
     order_ID = fields.Integer(string="Order number", default=0, help="Current Order Number shared across all POS in Multi Session")
     sync_server = fields.Char('Sync Server', default='')
+    run_ID = fields.Integer(string="Running count",
+                            help="Number of Multi-session starts. "
+                                 "It's incremented each time the last session in Multi-session is closed. "
+                                 "It's used to prevent synchronization of old orders")
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+
+    @api.multi
+    def action_pos_session_close(self):
+        res = super(PosSession, self).action_pos_session_close()
+        self.config_id.write({'multi_session_message_ID': 1})
+        active_sessions = self.env['pos.session'].search([('state', '!=', 'closed'), ('config_id.multi_session_id', '=', self.config_id.multi_session_id.id)])
+        if len(active_sessions) == 0:
+            self.config_id.multi_session_id.sudo().write({'order_ID': 0})
+            run_ID = self.config_id.multi_session_id.run_ID + 1
+            self.config_id.multi_session_id.sudo().write({'run_ID': run_ID})
+        return res
--- pos_multi_session_sync/models/pos_multi_session_sync_models.py
+++ pos_multi_session_sync/models/pos_multi_session_sync_models.py
@@ -14,8 +14,10 @@ class PosConfigSync(models.Model):
     _name = 'pos_multi_session_sync.pos'
 
     multi_session_ID = fields.Integer('Multi-session', index=True,
-                                       help='Set the same value for POSes where orders should be synced. Keep empty if this POS should not use syncing. Before updating it you need to close active session')
-    multi_session_message_ID = fields.Integer(index=True, string='Multi-session message')
+                                       help='Set the same value for POSes where orders should be synced. '
+                                            'Keep empty if this POS should not use syncing. '
+                                            'Before updating it you need to close active session')
+    multi_session_message_ID = fields.Integer(index=True, default=1, string="Last sent Multi-session message number")
     pos_ID = fields.Integer(index=True, string='POS')
     user_ID = fields.Integer(index=True)
 
@@ -24,10 +26,12 @@ class PosMultiSessionSync(models.Model):
     _name = 'pos_multi_session_sync.multi_session'
 
     multi_session_ID = fields.Integer('Multi-session', index=True,
-                                       help='Set the same value for POSes where orders should be synced. Keep empty if this POS should not use syncing. Before updating it you need to close active session')
+                                       help='Set the same value for POSes where orders should be synced. '
+                                            'Keep empty if this POS should not use syncing. '
+                                            'Before updating it you need to close active session')
     order_ID = fields.Integer(index=True, string='Order')
     dbname = fields.Char(index=True)
-
+    order_ids = fields.One2many('pos_multi_session_sync.order', 'multi_session_ID', 'Orders')
 
     @api.multi
     def on_update_message(self, message):
@@ -47,9 +51,6 @@ class PosMultiSessionSync(models.Model):
         self.ensure_one()
         client_revision_ID = message['data']['revision_ID']
         server_revision_ID = order.revision_ID
-        print "========="
-        print client_revision_ID
-        print server_revision_ID
         if not server_revision_ID:
             server_revision_ID = 1
         if client_revision_ID is not server_revision_ID:
@@ -64,33 +65,33 @@ class PosMultiSessionSync(models.Model):
         sequence_number = message['data']['sequence_number']
         order = self.env['pos_multi_session_sync.order'].search([('order_uid', '=', order_uid)])
         revision = self.check_order_revision(message, order)
+        run_ID = self.env['pos_multi_session_sync.order'].search([('order_uid', '=', order_uid)]).run_ID or False
         if not revision or (order and order.state == 'deleted'):
             return {'action': 'revision_error'}
         if order:  # order already exists
-            print "--------------if"
             order.write({
                 'order': json.dumps(message),
                 'revision_ID': order.revision_ID + 1,
+                'run_ID': message['data']['run_ID']
             })
         else:
-            print "--------------else"
             if self.order_ID + 1 != sequence_number:
                 sequence_number = self.order_ID + 1
                 message['data']['sequence_number'] = sequence_number
             order = order.create({
                 'order': json.dumps(message),
                 'order_uid': order_uid,
-                # 'multi_session_id': self.id,
+                'multi_session_ID': self.id,
+                'run_ID': run_ID,
             })
             self.write({'order_ID': sequence_number})
 
         revision_ID = order.revision_ID
+
         message['data']['revision_ID'] = revision_ID
-        print "--------------"
-        print revision_ID
-        print
         self.broadcast_message(message)
-        return {'action': 'update_revision_ID', 'revision_ID': revision_ID, 'order_ID': sequence_number}
+        return {'action': 'update_revision_ID', 'revision_ID': revision_ID,
+                'order_ID': sequence_number, 'run_ID': run_ID}
 
     @api.multi
     def get_sync_all(self, message):
@@ -99,23 +100,27 @@ class PosMultiSessionSync(models.Model):
         user_ID = self.env.context.get("user_ID")
         pos = self.env['pos_multi_session_sync.pos'] \
                   .search([('multi_session_ID', '=', self.multi_session_ID), ("pos_ID", "=", pos_ID)])
+        run_ID = message['data']['run_ID']
+        old_orders = self.order_ids.filtered(lambda x: x.state == "draft" and x.run_ID < run_ID)
+        if old_orders:
+            old_orders.write({'state': 'unpaid'})
+            self.write({'order_ID': 0})
         if not pos:
             pos = self.env['pos_multi_session_sync.pos'] \
-                .create({'multi_session_ID': self.multi_session_ID, 'id': pos_ID, 'user_ID': user_ID})
+                .create({'multi_session_ID': self.multi_session_ID, 'pos_ID': pos_ID, 'user_ID': user_ID})
         if pos.user_ID != user_ID:
             pos.user_ID = user_ID
+        pos.multi_session_message_ID = 0
         data = []
-        for order in self.env['pos_multi_session_sync.order'].search([('multi_session_ID', '=', self.id), ('state', '=', 'draft')]):
+        for order in self.env['pos_multi_session_sync.order'] \
+                         .search([('multi_session_ID', '=', self.id), ('state', '=', 'draft'),
+                                  ('run_ID', '=', run_ID)]):
             msg = json.loads(order.order)
-            msg['data']['message_ID'] = pos.multi_session_message_ID
+            msg['data']['message_ID'] = 0
             msg['data']['revision_ID'] = order.revision_ID
+            msg['data']['run_ID'] = run_ID
             data.append(msg)
         message = {'action': 'sync_all', 'orders': data, 'order_ID': self.order_ID}
-
-        poses = self.env['pos_multi_session_sync.pos'] \
-                    .search([('multi_session_ID', '=', self.multi_session_ID)])
-        for pos in poses:
-            pos.multi_session_message_ID = 0
         return message
 
     @api.multi
@@ -162,3 +167,8 @@ class PosMultiSessionSyncOrder(models.Model):
     state = fields.Selection([('draft', 'Draft'), ('deleted', 'Deleted'), ('unpaid', 'Unpaid and removed')], default='draft', index=True)
     revision_ID = fields.Integer(default=1, string="Revision", help="Number of updates received from clients")
     multi_session_ID = fields.Integer(default=0, string='Multi session')
+    pos_session_ID = fields.Integer(index=True, default=0, string='POS session')
+    run_ID = fields.Integer(index=True, string="Running count",
+                            help="Number of Multi-session starts. "
+                                 "It's incremented each time the last session in Multi-session is closed. "
+                                 "It's used to prevent synchronization of old orders")
