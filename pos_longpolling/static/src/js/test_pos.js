3bdf418e93060e6787af27166f0c6f00189da671
Your Name <kolushov@it-projects.info>
2017-10-17 15:24:37 +0500

    [CI] longpoll tests, [FIX] pos_ms tests

--- pos_longpolling/static/src/js/test_pos.js
+++ pos_longpolling/static/src/js/test_pos.js
@@ -1,20 +1,10 @@
-odoo.define('pos_multi_session.tour', function (require) {
+
+odoo.define('pos_longpolling.tour', function (require) {
     "use strict";
 
     var tour = require("web_tour.tour");
     var core = require('web.core');
     var _t = core._t;
-
-    function add_product_to_order(product_name) {
-        return [{
-            content: 'buy ' + product_name,
-            trigger: '.product-list .product-name:contains("' + product_name + '")',
-        }, {
-            content: 'the ' + product_name + ' have been added to the order',
-            trigger: '.order .product-name:contains("' + product_name + '")',
-//            run: function () {}, // it's a check
-        }];
-    }
 
     var steps = [{
             trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"], .oe_menu_toggler[data-menu-xmlid="point_of_sale.menu_point_root"]',
@@ -25,14 +15,12 @@ odoo.define('pos_multi_session.tour', function (require) {
             content: _t("<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>"),
             position: "bottom"
         }, {
-            content: 'waiting for loading to finish',
-            trigger: '.neworder-button > .fa-plus',
+            content: " Notification widget is visible",
+            trigger: ".js_poll_connected",
+            auto: true,
         }
     ];
 
-    steps = steps.concat(add_product_to_order('Ekomurz.nl'));
-    console.log(steps);
-
-    tour.register('open_pos_tour', { test: true, url: '/web' }, steps);
+    tour.register('longpoll_connection_tour', { test: true, url: '/web' }, steps);
 
 });
