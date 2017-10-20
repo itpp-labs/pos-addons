f6073f70ee291ff25a11977f831aa9de40db5622
ilmir <ilmir.karamov@gmail.com>
2017-10-20 14:06:01 +0500

    [CI] tour for 'Allow refund' feature

--- pos_disable_payment/static/src/js/tour.js
+++ pos_disable_payment/static/src/js/tour.js
@@ -9,6 +9,35 @@ odoo.define('pos_disable_payment.tour', function(require) {
     tour.register('pos_disable_payment_tour', {
         url: "/web",
     }, [{
+        trigger: '.o_app[data-menu-xmlid="base.menu_administration"], .oe_menu_toggler[data-menu-xmlid="base.menu_administration"]',
+        content: _t("Configuration options are available in the Settings app."),
+        position: "bottom"
+    }, {
+        trigger: '.oe_menu_leaf[data-menu=65]',
+        content: _t("Open the <b>Users</b> menu"),
+        position: "right"
+    }, {
+        trigger: '.o_list_view > tbody > tr',
+        content: _t("Click on the item"),
+        position: "bottom"
+    }, {
+        trigger: ".o_form_button_edit",
+        content: _t("Click to edit user"),
+        position: "bottom"
+    }, {
+        trigger: 'a[href="#notebook_page_12"]',
+        content: _t("Move on <b>Point of Sale</b> tab"),
+        position: "top"
+    }, {
+        trigger: 'label:contains("Allow refunds")',
+        extra_trigger: 'input[name="allow_refund"]:propChecked({disabled: true})',
+        content: _t("Uncheck the box <b>Allow refunds</b>"),
+        position: "right"
+    }, {
+        trigger: ".o_form_button_save",
+        content: _t("Save the changes"),
+        position: "bottom"
+    }, {
         trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"], .oe_menu_toggler[data-menu-xmlid="point_of_sale.menu_point_root"]',
         content: _t("Ready to launch your <b>point of sale</b>? <i>Click here</i>."),
         position: 'bottom',
@@ -16,12 +45,6 @@ odoo.define('pos_disable_payment.tour', function(require) {
         trigger: ".o_pos_kanban button.oe_kanban_action_button",
         content: _t("<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>"),
         position: "bottom"
-    }, {
-        content: " Username is visible",
-        trigger: ".username",
-        auto: true,
-    }
-
-       ]);
+    }]);
 
 });
