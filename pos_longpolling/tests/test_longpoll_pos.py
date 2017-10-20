d8746edb9d834eeb80c77bc462af64a189bb6141
Your Name <kolushov@it-projects.info>
2017-10-20 15:57:17 +0500

    [FIX] - adding delays in tests back

--- pos_longpolling/tests/test_longpoll_pos.py
+++ pos_longpolling/tests/test_longpoll_pos.py
@@ -8,7 +8,8 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_longpolling_pos(self):
+        # without a delay there might be problems on the steps whilst opening a POS
         self.phantom_js("/web",
-                        "odoo.__DEBUG__.services['web_tour.tour'].run('longpoll_connection_tour')",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('longpoll_connection_tour', 1000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.longpoll_connection_tour.ready",
                         login="admin")
--- pos_multi_session/tests/test_pos.py
+++ pos_multi_session/tests/test_pos.py
@@ -8,7 +8,8 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_open_pos(self):
+        # without a delay there might be problems on the steps whilst opening a POS
         self.phantom_js("/web",
-                        "odoo.__DEBUG__.services['web_tour.tour'].run('tour_pos_multi_session')",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('tour_pos_multi_session', 1000)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.tour_pos_multi_session.ready",
                         login="admin")
