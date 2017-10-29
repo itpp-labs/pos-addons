c06eb4de85553b83e621b166c09d0cc4c562cca0
ilmir <ilmir.karamov@gmail.com>
2017-10-30 01:24:07 +0500

    [CI] additional trigger for tour

--- pos_disable_payment/static/src/js/tour.js
+++ pos_disable_payment/static/src/js/tour.js
@@ -47,6 +47,10 @@ odoo.define('pos_disable_payment.tour', function(require) {
         content: _t("<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>"),
         position: "bottom"
     }, {
+        content: "Switch to table or make dummy action",
+        trigger: '.table:not(.oe_invisible .neworder-button), .order-button.selected',
+        position: "bottom"
+    }, {
         trigger: '.product-list .product',
         content: _t("Add the first product"),
         position: "top"
