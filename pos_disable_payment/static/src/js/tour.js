2d8d01b2833aee64ae4eae00181103cbcde4c28c
ilmir <ilmir.karamov@gmail.com>
2017-11-01 14:39:33 +0500

    [LINT] pylint warnings

--- pos_disable_payment/static/src/js/tour.js
+++ pos_disable_payment/static/src/js/tour.js
@@ -56,12 +56,12 @@ odoo.define('pos_disable_payment.tour', function(require) {
         content: "Hidden",
         trigger: '.pads',
         run: function(){
-            if (!$('.numpad-minus').css('visibility') == 'hidden') {
+            if (!$('.numpad-minus').css('visibility') === 'hidden') {
                 console.log("error", "The button is not hidden");
-                }
+            }
             else {
                 console.log("Everything is ok");
-                }
+            }
         }
     }
 
