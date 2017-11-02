61c19106a9dedca91c7fd064f9a858bc6c868de9
GabbasovDinar <gabbasov@it-projects.info>
2017-11-02 16:20:19 +0500

    [IMP] remove not maintaining modules

--- pos_printer_network/static/src/js/network_printer.js
+++ pos_printer_network/static/src/js/network_printer.js
@@ -11,7 +11,7 @@ odoo.define('pos_restaurant.network_printer', function (require) {
     var chrome = require('point_of_sale.chrome');
     var PopupWidget = require('point_of_sale.popups');
     var QWeb = core.qweb;
-    var mixins = core.mixins;
+    var mixins = requere("web.mixins");
 
     var _t = core._t;
 
