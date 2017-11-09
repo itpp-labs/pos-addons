8620d33407e4a08aca472ea4c001b702b6b7759f
root <gabbasov@it-projects.info>
2017-11-09 10:29:16 +0000

    [PORT] porting from 10.0 to 11.0

--- pos_order_cancel/models/models.py
+++ pos_order_cancel/models/models.py
@@ -57,7 +57,7 @@ class PosOrder(models.Model):
         order = super(PosOrder, self)._process_order(pos_order)
         if 'is_cancelled' in pos_order and pos_order['is_cancelled'] is True:
             if pos_order['reason']:
-                order.cancellation_reason = pos_order['reason'].encode('utf-8').strip(" \t\n")
+                order.cancellation_reason = pos_order['reason'].encode('utf-8')
             order.is_cancelled = True
         return order
 
