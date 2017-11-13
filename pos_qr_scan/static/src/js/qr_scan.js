ca7d163daa69c68a6e3344ec28a0548ce8885d0f
Your Name <kolushov@it-projects.info>
2017-11-13 15:32:08 +0500

    [FIX] error on second popup openning

--- pos_qr_scan/static/src/js/qr_scan.js
+++ pos_qr_scan/static/src/js/qr_scan.js
@@ -32,6 +32,34 @@ odoo.define('pos_qr_scan', function(require){
         show: function (options) {
             var self = this;
             this._super(options);
+            this.start_script();
+        },
+        click_cancel: function(){
+            this.var_scanner.stop();
+            this._super(arguments);
+        },
+        start_script: function() {
+            this.var_scanner = new Instascan.Scanner({video: document.getElementById('preview')});
+            var scanner = this.var_scanner;
+            scanner.addListener('scan', function (content) {
+                console.log(content);
+                var new_scan = document.createElement('a');
+                new_scan.setAttribute("href", content);
+                var par = document.createElement("p");
+                par.innerHTML = content;
+                new_scan.appendChild(par);
+                $('.sidebar > .body').append(new_scan);
+                scanner.stop()
+            });
+            Instascan.Camera.getCameras().then(function (cameras) {
+                if (cameras.length > 0) {
+                    scanner.start(cameras[0]);
+                } else {
+                    console.error('No cameras found.');
+                }
+            }).catch(function (e) {
+                console.error(e);
+            });
         },
     });
     gui.define_popup({name:'qr_scan', widget: QrScanPopupWidget});
