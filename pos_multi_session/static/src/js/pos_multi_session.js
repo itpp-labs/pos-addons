07e124d38d13bd89bb81bb1e567d9dee7869b8b7
Your Name <kolushov@it-projects.info>
2017-11-28 13:25:46 +0500

    [FIX] endless errors if sync server is defined incorrectly

--- pos_multi_session/static/src/js/pos_multi_session.js
+++ pos_multi_session/static/src/js/pos_multi_session.js
@@ -609,7 +609,7 @@ odoo.define('pos_multi_session', function(require){
                 if (self.pos.debug){
                     console.log('MS', self.pos.config.name, 'failed request #'+current_send_number+':', error.message);
                 }
-                if(error.data.type === "xhrerror") {
+                if(error.data.type === "xhrerror" || (error.data.name && error.data.name.search('NotFound'))) {
                     self.client_online = false;
                     e.preventDefault();
                     self.pos.sync_bus.longpolling_connection.network_is_off();
