c45184f9a615f5903883e389b6527db87a22f38e
Your Name <kolushov@it-projects.info>
2018-01-17 16:25:38 +0500

    [FIX] error @class->hasclass [CI] table choosing in case of a restaurant

--- pos_debt_notebook/static/src/js/test_pos_debt.js
+++ pos_debt_notebook/static/src/js/test_pos_debt.js
@@ -15,6 +15,10 @@ odoo.define('pos_debt_notebook.tour', function (require) {
             content: _t("<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>"),
             position: "bottom"
         }, {
+            content: "Switch to table or make dummy action",
+            trigger: '.table:not(.oe_invisible .neworder-button), .order-button.selected',
+            position: "bottom"
+        }, {
             content: 'waiting for loading to finish',
             trigger: '.neworder-button > .fa-plus',
         }];
