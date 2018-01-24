09aea4166cdb2a57cc4df66fa2c8c8ff2ccb1618
ILMIR <karamov@it-projects.info>
2018-01-24 19:28:30 +0500

    [ADD] pos_disable_payment: demo url

--- pos_disable_payment/__openerp__.py
+++ pos_disable_payment/__openerp__.py
@@ -6,6 +6,7 @@
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
     'category': 'Point Of Sale',
+    'live_test_url': 'http://apps.it-projects.info/shop/product/pos-multi-session?version=11.0',
     "support": "apps@it-projects.info",
     'website': 'https://yelizariev.github.io',
     'depends': ['point_of_sale'],
