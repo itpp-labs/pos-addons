75264b9fb6fe21b88556b98db8b0143d78c46de9
Your Name <kolushov@it-projects.info>
2018-01-31 15:07:33 +0500

    [CI]

--- pos_product_available/static/src/js/test_pos_quantities.js
+++ pos_product_available/static/src/js/test_pos_quantities.js
@@ -7,10 +7,6 @@ odoo.define('pos_product_available.tour', function (require) {
 
     function open_pos_neworder() {
         return [{
-            trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"], .oe_menu_toggler[data-menu-xmlid="point_of_sale.menu_point_root"]',
-            content: _t("Ready to launch your <b>point of sale</b>? <i>Click here</i>."),
-            position: 'bottom',
-        }, {
             trigger: ".o_pos_kanban button.oe_kanban_action_button",
             content: _t("<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>"),
             position: "bottom"
@@ -82,7 +78,11 @@ odoo.define('pos_product_available.tour', function (require) {
         }];
     }
 
-    var steps = [];
+    var steps = [{
+            trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"], .oe_menu_toggler[data-menu-xmlid="point_of_sale.menu_point_root"]',
+            content: _t("Ready to launch your <b>point of sale</b>? <i>Click here</i>."),
+            position: 'bottom',
+        }];
     steps = steps.concat(open_pos_neworder());
     steps = steps.concat(add_product_to_order('Zucchini'));
     steps = steps.concat(payment('Cash (USD)'));
