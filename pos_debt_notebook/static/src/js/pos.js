4e295c573a12c405f2ff4f95907cad525c1c78d3
Ivan Yelizariev <yelizariev@it-projects.info>
2018-02-18 03:46:28 +0500

    Revert "[FIX] bug on credits transfering"
    This reverts commit da4fab82cf1b83cc193ef1135e8a5d086472ca12.

--- pos_debt_notebook/static/src/js/pos.js
+++ pos_debt_notebook/static/src/js/pos.js
@@ -205,18 +205,8 @@ odoo.define('pos_debt_notebook.pos', function (require) {
         },
         get_summary_for_cashregister: function(cashregister) {
             var self = this;
-            var debt_updates = _.find(this.paymentlines.models, function(pl){
-                return pl.amount < 0;
-            });
-            var cash_out = cashregister.journal.pos_cash_out;
-            // In case of Credit/debt updating, there is a paymentline with amount = change, so for any amount debt_limit won't be exceeded.
-            // Thus it is necessary to ignore the change in this case.
-            // Also change is ignored for journals that may be cashed out.
             return _.reduce(this.paymentlines.models, function(memo, pl){
                 if (pl.cashregister.journal_id[0] === cashregister.journal.id) {
-                    if (cash_out || debt_updates) {
-                        return memo + pl.amount;
-                    }
                     return memo + pl.amount - self.get_change(pl);
                 }
                 return memo;
@@ -355,8 +345,8 @@ odoo.define('pos_debt_notebook.pos', function (require) {
                 });
                 return;
             }
-            var exceeding_debts = this.exceeding_debts_check();
-            if (client && exceeding_debts) {
+            var exceeding_debts = this.exceeding_debts_check()
+            if (client && debt_amount > 0 && exceeding_debts) {
                 this.gui.show_popup('error', {
                     'title': _t('Max Debt exceeded'),
                     'body': _t('You cannot sell products on credit journal ' + exceeding_debts + ' to the customer because its max debt value will be exceeded.')
