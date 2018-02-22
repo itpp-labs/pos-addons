ec065907a192c2015a18fd4a1a6b1855d9f2f1d1
GabbasovDinar <gabbasov@it-projects.info>
2018-02-22 16:12:04 +0500

    [REF] refactoring code

--- pos_mobile_restaurant/static/src/js/floors.js
+++ pos_mobile_restaurant/static/src/js/floors.js
@@ -18,6 +18,23 @@ odoo.define('pos_mobile_restaurant.floors', function (require) {
             var floor = this.pos.floors_by_id[id];
             var slide = $('div[data-id='+id+'][id=slide-floor]').index();
             this.chrome.swiper_floors.slideTo(slide);
+        },
+        renderElement: function(){
+            this._super();
+            var map = this.$el.find('.floor-map');
+            var slide = this.get_floor_slide_by_id(this.floor.id);
+            if (slide.length) {
+                // replace exist slide
+                slide.find('.floor-map').replaceWith(map);
+            } else {
+                // append new slide
+                this.chrome.swiper_floors.appendSlide('<div class="swiper-slide slide-floor" id="slide-floor" data-id='+this.floor.id+'></div>');
+                slide = this.get_floor_slide_by_id(this.floor.id);
+                slide.append(map);
+            }
+        },
+        get_floor_slide_by_id: function(id) {
+            return $('.swiper-slide.slide-floor[data-id='+ id +']');
         }
     });
 
