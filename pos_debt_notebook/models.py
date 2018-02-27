2d599707e02ef5eb2d7e0fa5f7d811a659863e3f
KolushovAlexandr <kolushov@it-projects.info>
2018-02-27 14:26:33 +0500

    [REF] search(id) -> browse

--- pos_debt_notebook/models.py
+++ pos_debt_notebook/models.py
@@ -101,7 +101,7 @@ class ResPartner(models.Model):
                 )
                 for r2 in records:
                     r2['date'] = self._get_date_formats(r2['date'])
-                    r2['journal_code'] = self.env['account.journal'].search([('id', '=', r2['journal_id'][0])]).code
+                    r2['journal_code'] = self.env['account.journal'].browse(r2['journal_id'][0]).code
                 data['history'] = records
             data['debts'] = self.compute_debts_by_journals(r.id)
             data['records_count'] = self.env['report.pos.debt'].search_count(domain)
@@ -438,13 +438,13 @@ class PosOrder(models.Model):
     def _process_order(self, pos_order):
         credit_updates = []
         for payment in pos_order['statement_ids']:
-            journal = self.env['account.journal'].search([('id', '=', payment[2]['journal_id'])])
+            journal = self.env['account.journal'].browse(payment[2]['journal_id'])
             if journal.credits_via_discount:
                 amount = float(payment[2]['amount'])
                 product_list = list()
                 for o_line in pos_order['lines']:
                     o_line = o_line[2]
-                    name = self.env['product.product'].search([('id', '=', o_line['product_id'])]).name
+                    name = self.env['product.product'].browse(o_line['product_id']).name
                     product_list.append('%s(%s * %s) + ' % (name, o_line['qty'], o_line['price_unit']))
                 product_list = ''.join(product_list).strip(' + ')
                 credit_updates.append({'journal_id': payment[2]['journal_id'],
