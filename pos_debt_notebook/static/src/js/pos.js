98bc9fb695dac42a117f0c815611435b4eed5789
KolushovAlexandr <kolushov@it-projects.info>
2018-03-03 00:37:20 +0500

    [FIX] FIXME: find solution that doesnt depend on UI (width of progress bar, etc.)

--- pos_debt_notebook/static/src/js/pos.js
+++ pos_debt_notebook/static/src/js/pos.js
@@ -33,10 +33,7 @@ odoo.define('pos_debt_notebook.pos', function (require) {
 
             var done = new $.Deferred();
 
-            // FIXME: find solution that doesn't depend on UI (width of progress bar, etc.)
-            // .progressbar has constant width 400px
-            var progress = $('.progressbar .progress').width();
-            progress = (progress + (400 - progress) / 2) / 400;
+            var progress = (this.models.length - 0.5) / this.models.length;
             this.chrome.loading_message(_t('Loading Credits data'), progress);
             this.reload_debts(partner_ids, 0, {"postpone": false}).then(function(){
                 done.resolve();
