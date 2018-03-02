e33484d0142d10cb8c05f06f39c35b917f4cc1f9
David Arnold <dar@xoe.solutions>
2018-03-02 03:45:11 -0500

    [10.0][FIX] pos_disable_payment: [l18n] fix ambigous naming (+de, +es) (#600)

--- pos_disable_payment/models.py
+++ pos_disable_payment/models.py
@@ -9,7 +9,7 @@ class ResUsers(models.Model):
     allow_delete_order = fields.Boolean('Allow remove non-empty order', default=True)
     allow_discount = fields.Boolean('Allow discount', default=True)
     allow_edit_price = fields.Boolean('Allow edit price', default=True)
-    allow_decrease_amount = fields.Boolean('Allow decrease order line', default=True)
+    allow_decrease_amount = fields.Boolean('Allow decrease quantity on order line', default=True)
     allow_decrease_kitchen_only = fields.Boolean('Allow change Qty for kitchen orders', default=False)
     allow_delete_order_line = fields.Boolean('Allow remove order line', default=True)
     allow_create_order_line = fields.Boolean('Allow create order line', default=True)
