b32a784512e42b987c08888f0117efb52ead977a
Ilmir <ilmir.karamov@gmail.com>
2018-03-07 14:03:38 +0500

    [9.0][DOC] pos_multi_session: demo url

--- pos_multi_session/__openerp__.py
+++ pos_multi_session/__openerp__.py
@@ -3,6 +3,7 @@
     "name": """Sync POS orders across multiple sessions""",
     "summary": """Use multiple POS for handling orders""",
     "category": "Point Of Sale",
+    "live_test_url": "http://apps.it-projects.info/shop/product/pos-multi-session?version=9.0",
     "images": ["images/pos-multi-session.png"],
     "version": "3.0.2",
     "application": False,
@@ -36,4 +37,17 @@
 
     "auto_install": False,
     "installable": True,
+
+    "demo_title": "Sync POS orders across multiple sessions",
+    "demo_addons": [
+        "pos_disable_payment",
+        "pos_multi_session_restaurant",
+    ],
+    "demo_addons_hidden": [
+    ],
+    "demo_url": "pos-multi-session",
+    "demo_summary": "Use multiple POSes for handling orders",
+    "demo_images": [
+        "images/pos-multi-session.png",
+    ]
 }
