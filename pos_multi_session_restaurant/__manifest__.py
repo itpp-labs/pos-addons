9b96f183211387e13d0ff40f6b3b3dbb25d9f1ba
GabbasovDinar <gabbasov@it-projects.info>
2018-04-02 15:04:26 +0500

    [DOC] add information of license for pos_multi_session_restaurant

--- pos_multi_session_restaurant/__manifest__.py
+++ pos_multi_session_restaurant/__manifest__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Ilmir Karamov <https://it-projects.info/team/ilmir-k>
+# Copyright 2017-2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
 {
     "name": """Sync restaurant orders""",
     "summary": """Staff get order details immediately after waiter taps on tablet""",
