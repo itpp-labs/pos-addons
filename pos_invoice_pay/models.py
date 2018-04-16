544a2f218e6c7d662a1c4e75c8c8d23ac7811b60
KolushovAlexandr <kolushov@it-projects.info>
2018-04-16 15:11:14 +0500

    [DOC] changelog and license

--- pos_invoice_pay/models.py
+++ pos_invoice_pay/models.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Artyom Losev
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
 from odoo import api, models, fields
 
 SO_CHANNEL = 'pos_sale_orders'
--- pos_invoice_pay/report/report.py
+++ pos_invoice_pay/report/report.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Artyom Losev
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
 import datetime
 from odoo import api, fields, models
 
