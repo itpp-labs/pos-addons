b037f0c59a96ec189ca7df36ddfe6648f73d1c90
GabbasovDinar <gabbasov@it-projects.info>
2018-04-20 13:34:40 +0500

    [LINT] fix lint errors

--- pos_fiscal_floor/__openerp__.py
+++ pos_fiscal_floor/__openerp__.py
@@ -1,15 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'POS fiscal floor',
-    'version': '1.0.1',
-    'author': 'IT-Projects LLC',
-    'license': 'LGPL-3',
-    'category': 'Point Of Sale',
-    "support": "apps@it-projects.info",
-    'website': 'https://yelizariev.github.io',
-    'description': "Add Fiscal Position to POS floor model",
-    'data': ['views.xml'],
-    'depends': ['base', 'point_of_sale', 'pos_restaurant'],
-    'installable': True,
+    "name": """POS fiscal floor""",
+    "summary": """Add Fiscal Position to POS floor model""",
+    "category": "Point Of Sale",
+    # "live_test_url": "",
+    "images": [],
+    "version": "10.0.1.0.1",
+    "application": False,
 
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/yelizariev",
+    "license": "LGPL-3",
+
+    "depends": [
+        "pos_restaurant",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views.xml",
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
 }
