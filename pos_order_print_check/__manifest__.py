0acd833fb300f6615c86c0c19fdb679c00c42844
Dinar <gabbasov@it-projects.info>
2018-04-24 16:26:56 +0500

    [10.0][NEW] Check the connection to PosBox before printing kitchen orders (it-projects-llc/addons-dev#416)
    * [10.0][NEW] Check connection with PosBox befor printing orders
    
    * [FIX] view name
    
    * [DOC] Update for Readme
    
    * [DOC] update for manifest file
    
    * [DOC] Update for index.rst
    
    * [FIX] warning message

---
+++ pos_order_print_check/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Check PosBox Connection""",
+    "summary": """Check the connection to PosBox before printing kitchen orders""",
+    "category": "Point of Sale",
+    # "live_test_url": "",
+    "images": [],
+    "version": "10.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Dinar Gabbasov",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/GabbasovDinar",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "pos_restaurant",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/pos_order_print_check_template.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
