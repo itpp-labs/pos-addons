6118a77e625946ec72733b80ca03b2f373d0b8f2
GabbasovDinar <gabbasov@it-projects.info>
2018-04-26 12:56:45 +0500

    [IMP] Add a missed order to queue

--- hw_printer_network/controllers/hw_printer_network_controller.py
+++ hw_printer_network/controllers/hw_printer_network_controller.py
@@ -123,7 +123,9 @@ class EscposNetworkDriver(EscposDriver):
                             _logger.info('Done printing XML receipt on printer %s', network_printer_ip)
                         else:
                             _logger.error('xml_receipt: printer offline!')
-                            # TODO: do something with the missed order?
+                            # add a missed order to queue
+                            time.sleep(3)
+                            self.queue.put((timestamp, task, data))
                 elif task == 'printstatus':
                     pass
                 elif task == 'status':
