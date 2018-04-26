996ef6b131624a63efcbcd9f91f2d51a6c5d4cfa
GabbasovDinar <gabbasov@it-projects.info>
2018-04-26 13:19:07 +0500

    [LINT] fix lint error

--- hw_printer_network/__openerp__.py
+++ hw_printer_network/__openerp__.py
@@ -8,7 +8,7 @@
     "summary": """Hardware Driver for Network Printers""",
     "category": "Point of Sale",
     "images": [],
-    "version": "2.0.1",
+    "version": "10.0.2.0.1",
     "application": False,
 
     "author": "IT-Projects LLC, Dinar Gabbasov",
