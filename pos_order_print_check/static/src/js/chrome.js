6cdd43232cb6659c0303e3c6f3d0af0e9d2ff8e9
Dinar <gabbasov@it-projects.info>
2018-05-04 18:03:17 +0500

    [IMP] show the warning message before close/update page (it-projects-llc/addons-dev#427)
    [IMP] show the warning message before close/update page
    
    [FIX] compatibility with pos_mobile

--- pos_order_print_check/static/src/js/chrome.js
+++ pos_order_print_check/static/src/js/chrome.js
@@ -39,6 +39,10 @@ odoo.define('pos_order_print_check.chrome', function (require) {
             if (this.order_print_quantity !== sum) {
                 this.order_print_quantity = sum;
                 this.renderElement();
+                // if use the pos_mobile module
+                if (this.pos.is_mobile) {
+                    $($('.pos-rightheader .oe_status')[0]).css({'margin-right': '70px'});
+                }
                 if (this.show_warning_message ) {
                     this.pos.gui.show_popup('error',{
                         'title': _t('Warning'),
@@ -48,9 +52,43 @@ odoo.define('pos_order_print_check.chrome', function (require) {
                 }
                 if (this.order_print_quantity === 0) {
                     this.show_warning_message = true;
+                    $(window).off('beforeunload', this.unload);
+                } else {
+                    this.unloader();
                 }
             }
+        },
+        unloader: function(){
+            var self = this;
+            $(window).on('beforeunload', self.unload);
+            $('a').on('click', function(){
+                self.resetUnload();
+            });
+            $(document).on('submit', 'form', function(){o.resetUnload});
+            $(document).on('keydown', function(event){
+                if((event.ctrlKey && event.keyCode == 116) || event.keyCode == 116){
+                    self.resetUnload();
+                }
+            });
+        },
+        unload: function(evt){
+            var message = _t("The changes you have made will not be saved.");
+            if (typeof evt == "undefined") {
+                evt = window.event;
+            }
+            if (evt) {
+                evt.returnValue = message;
+            }
+            return message;
+        },
+        resetUnload: function() {
+            var self = this;
+            $(window).off('beforeunload', self.unload);
+            setTimeout(function(){
+                $(window).on('beforeunload', self.unload);
+            }, 2000);
         }
     });
+
     return chrome;
 });
