9ab68391768e538e2e7c3f7849c8e3620ba3bee7
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2018-05-08 19:34:54 -0600

    Fixes #690

--- pos_debt_notebook/wizard/pos_credit_invoices.py
+++ pos_debt_notebook/wizard/pos_credit_invoices.py
@@ -66,6 +66,7 @@ class PosCreditInvoices(models.TransientModel):
             if not line.amount:
                 continue
             self.env['account.invoice'].create({
+                'type': 'out_invoice',
                 'partner_id': line.partner_id.id,
                 'invoice_line_ids': [
                     (0, None, {
