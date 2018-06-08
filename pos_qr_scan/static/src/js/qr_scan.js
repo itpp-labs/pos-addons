2b45afee0bb983310b90f3117c50ee353a4decde
KolushovAlexandr <kolushov@it-projects.info>
2018-06-08 19:07:53 +0500

    [ADD] camera selection

--- pos_qr_scan/static/src/js/qr_scan.js
+++ pos_qr_scan/static/src/js/qr_scan.js
@@ -40,27 +40,35 @@ odoo.define('pos_qr_scan', function(require){
             var new_scan = document.createElement('div');
             new_scan.className = 'button qr-content'
             new_scan.innerHTML = content;
-            $('.sidebar > .body').append(new_scan);
+            return $('.transparent_sidebar > .body').append(new_scan);
         },
         generate_qr_scanner: function() {
+            var self = this;
             this.var_scanner = new Instascan.Scanner({video: document.getElementById('preview')});
             var scanner = this.var_scanner;
-            var qr_scan_popup = self.posmodel.gui.popup_instances.qr_scan;
-            if (self.posmodel.get_order().auth_code) {
-                var old_content = self.posmodel.get_order().auth_code;
+            var qr_scan_popup = this.pos.gui.popup_instances.qr_scan;
+            if (this.pos.get_order().auth_code) {
+                var old_content = this.pos.get_order().auth_code;
                 qr_scan_popup.add_button(old_content);
             }
+            $('.popup.popup-qr_scan .preview-container').on('click',function(){
+                qr_scan_popup.click_cancel();
+            });
             scanner.addListener('scan', function (content) {
                 console.log(content);
                 qr_scan_popup.add_button(content);
-                self.posmodel.get_order().auth_code = content;
-                console.log(content)
-//                scanner.stop();
-//                qr_scan_popup.click_cancel();
+                self.pos.get_order().auth_code = content;
+                console.log(content);
             });
             Instascan.Camera.getCameras().then(function (cameras) {
                 if (cameras.length > 0) {
                     scanner.start(cameras[0]);
+                    for (var i = 0; i < cameras.length; i++) {
+                        self.add_button(cameras[i].name).off().on('click',function(){
+                            self.var_scanner.stop();
+                            scanner.start(cameras[i]);
+                        });
+                    };
                 } else {
                     console.error('No cameras found.');
                 }
