f19c3d4dcbec895b7aa11bb4cfbb6ba7ab2b7bba
KolushovAlexandr <kolushov@it-projects.info>
2018-06-21 13:24:44 +0500

    [DOC] licenses for base_attendance

--- base_attendance/static/src/js/test_kiosk_tour.js
+++ base_attendance/static/src/js/test_kiosk_tour.js
@@ -1,3 +1,5 @@
+/* Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+   License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html). */
 odoo.define('base_attendance.tour', function (require) {
     "use strict";
 
--- base_attendance/tests/__init__.py
+++ base_attendance/tests/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from . import test_hr_attendance_constraints
 from . import test_hr_attendance_process
