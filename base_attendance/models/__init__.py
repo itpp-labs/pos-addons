a1ed57b85c6eba9ae3e77af67aef685f1c77b30b
Ivan Yelizariev <yelizariev@it-projects.info>
2018-06-21 16:14:46 +0500

    [REM] base_attendance will be published in misc-addons

--- base_attendance/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
-from . import res_attendance
-from . import res_partner
-from . import res_config
--- base_attendance/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
-from . import test_hr_attendance_constraints
-from . import test_hr_attendance_process
-from . import test_kiosk_tour
