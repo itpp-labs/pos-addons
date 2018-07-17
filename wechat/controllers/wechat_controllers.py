aa58a49c86bb3f7f9cadf5e1821346b776be341e
Ivan Yelizariev <yelizariev@it-projects.info>
2018-07-17 19:20:14 +0500

    :bomb: wechat callback receives xml

--- wechat/controllers/wechat_controllers.py
+++ wechat/controllers/wechat_controllers.py
@@ -1,7 +1,9 @@
 # Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+from lxml import etree
 import logging
+
 from odoo import http
 from odoo.http import request
 import json
@@ -10,12 +12,21 @@ import requests
 _logger = logging.getLogger(__name__)
 
 
+
 class WechatController(http.Controller):
 
     @http.route('/wechat/callback', methods=['POST'], auth='public', type='http', csrf=False)
-    def micropay(self, **kwargs):
-        _logger.debug('/wechat/callback request data: %s', kwargs)
-        res = request.env['wechat.order'].sudo().on_notification(kwargs)
+    def micropay(self):
+        xml_raw = request.httprequest.get_data().decode(request.httprequest.charset)
+        _logger.debug('/wechat/callback request data: %s\nheaders %s: ', xml_raw, request.httprequest.headers)
+
+        # convert xml to object
+        xml = etree.fromstring(xml_raw)
+        data = {}
+        for child in xml:
+            data[child.tag] = child.text
+
+        res = request.env['wechat.order'].sudo().on_notification(data)
 
         if res is not False:
             return """<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>"""
