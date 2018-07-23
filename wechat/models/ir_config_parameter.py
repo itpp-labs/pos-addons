03bbe96c219b6f68c83e155bd18e57f2cbac4301
GabbasovDinar <gabbasov@it-projects.info>
2018-07-23 20:15:48 +0500

    :green_heart: fix lint errors

--- wechat/models/ir_config_parameter.py
+++ wechat/models/ir_config_parameter.py
@@ -52,11 +52,11 @@ class Param(models.Model):
         if sandbox:
             _logger.info('Sandbox Mode is used for WeChat API')
         _logger.debug('WeChat Credentials: miniprogram_app_id=%s, miniprogram_app_secret=%s, mch_id=%s, sub_mch_id=%s, sandbox mode is %s',
-            self.get_param('wechat.miniprogram_app_id', ''),
-            '%s...' % self.get_param('wechat.miniprogram_app_secret', '')[:5],
-            self.get_param('wechat.mch_id', ''),
-            self.get_param('wechat.sub_mch_id', ''),
-            sandbox
+                      self.get_param('wechat.miniprogram_app_id', ''),
+                      '%s...' % self.get_param('wechat.miniprogram_app_secret', '')[:5],
+                      self.get_param('wechat.mch_id', ''),
+                      self.get_param('wechat.sub_mch_id', ''),
+                      sandbox
                       )
         return WeChatPay(
             self.get_param('wechat.miniprogram_app_id', ''),
