a00455d55473a6ca6424874758f948be2cf34530
Ivan Yelizariev <yelizariev@it-projects.info>
2018-07-27 15:47:41 +0500

    :fire_engine: fix installation problems

--- pos_alipay/models/__init__.py
+++ pos_alipay/models/__init__.py
@@ -1,6 +1,5 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from . import alipay_pos
-from . import alipay_scan
 from . import alipay_order
 from . import pos_order
 from . import pos_config
--- pos_alipay/tests/__init__.py
+++ pos_alipay/tests/__init__.py
@@ -1,3 +1,2 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-from . import test_scan
-from . import test_alipay_order
+from . import test_alipay
