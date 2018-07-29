ea6fade78db849b104fb4d4a78c728e42d566491
GabbasovDinar <gabbasov@it-projects.info>
2018-07-29 14:22:20 +0500

    :memo: pos_wechat_miniprogram new module

---
+++ pos_wechat_miniprogram/__manifest__.py
@@ -0,0 +1,34 @@
+# Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Integrate POS with WeChat mini-program""",
+    "summary": """Integrate POS with WeChat mini-program""",
+    "category": "Point of Sale",
+    # "live_test_url": "",
+    "images": [],
+    "version": "11.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Dinar Gabbasov",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/GabbasovDinar",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        "wechat_miniprogram",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "security/wechat_security.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+
+    "auto_install": False,
+    "installable": True,
+}
