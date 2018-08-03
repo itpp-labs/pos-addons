421f45fe1897c1c878daba3560f33b6918d18ecc
KolushovAlexandr <kolushov@it-projects.info>
2018-08-03 14:32:49 +0500

    :ambulance: issue related to debt loading after creating a new partner

--- pos_debt_notebook/__openerp__.py
+++ pos_debt_notebook/__openerp__.py
@@ -16,7 +16,7 @@
     'category': 'Point Of Sale',
     'live_test_url': 'http://apps.it-projects.info/shop/product/pos-debt-notebook?version=10.0',
     "images": ['images/debt_notebook.png'],
-    'version': '10.0.5.0.1',
+    'version': '10.0.5.0.2',
 
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     "support": "apps@it-projects.info",
