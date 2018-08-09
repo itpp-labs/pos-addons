ba68b4637098ac541216914358aa54234593d2f5
ILMIR <karamov@it-projects.info>
2018-08-09 16:56:06 +0500

    :rainbow: fix violation

--- pos_invoice_pay/__manifest__.py
+++ pos_invoice_pay/__manifest__.py
@@ -1,6 +1,3 @@
-# Copyright 2017 Artyom Losev
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-=======
 # Copyright 2018 Artyom Losev
 # Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
