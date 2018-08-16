fad27a278c21727081491c08b6ecf6a83cdb2d50
KolushovAlexandr <kolushov@it-projects.info>
2018-08-16 14:19:12 +0500

    :book: about autolock

--- pos_logout/models/logout_config.py
+++ pos_logout/models/logout_config.py
@@ -8,4 +8,4 @@ from odoo import models
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    logout_interval = fields.Integer(string='Screen Autolock', default=0, help="Interval after the last activity before screen is blocked. Zero if autolocking is not needed")
+    logout_interval = fields.Integer(string='Screen Autolock', default=0, help="The last activity interval to activate the automatic screen lock. Zero if autolocking is not needed")
