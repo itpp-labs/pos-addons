4a5686605680bbd45d7cff4bec0657a8a9efd836
Dinar <gabbasov@it-projects.info>
2018-08-22 16:55:19 +0500

    :memo: plan of new module - Tencent Cloud SMS API (it-projects-llc/addons-dev#539)
    * :memo: plan of new module
    
    * :fire_engine: add test_send_message
    
    * :fire_engine: test_send_message
    
    * :fire_engine: fix test_send_message
    
    * :memo: fix doc
    
    * :fire_engine: fix test_send_message
    
    * :fire_engine: fix test_send_message
    
    * :fire_engine: fix patch for test_send_message
    
    * :fire_engine: new test_send_group_message
    
    * :lock: add rules
    
    * :green_heart:
    
    * :fire: fix depends
    
    * :memo:
    
    * :bomb: add new fields and refactoring code
    
    * :bomb: remove old code
    
    * :rose: change warning message
    
    * :lock: change access
    
    * :tv: add view
    
    * :memo: update usage instruction
    
    * :tv: fix view
    
    * :fire: bool object has no attribute 'split'
    
    * :bomb: return sms object
    
    * :bomb: return sms id
    
    * :fire_engine: fix return values
    
    * :fire: refactoring tests
    
    * :fire_engine: passing tests and refactoring
    
    * :tv: fix view
    
    * :fire: fix tests
    
    * :fire_engine: passing tests and refactoring code
    
    * :fire_engine: passing tests
    
    * :rose: logs messages
    
    * :memo: update usage instruction
    
    * :book: fix the format in index.rst

---
+++ qcloud_sms/__manifest__.py
@@ -0,0 +1,34 @@
+# Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Tencent Cloud SMS API""",
+    "summary": """Technical module to intergrate odoo with Tencent Cloud SMS""",
+    "category": "Hidden",
+    # "live_test_url": "",
+    "images": [],
+    "version": "11.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Dinar Gabbasov",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/GabbasovDinar",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        'base',
+    ],
+    "external_dependencies": {"python": [
+        'qcloudsms_py',
+        'phonenumbers',
+    ], "bin": []},
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_config.xml",
+    ],
+    "qweb": [],
+
+    "auto_install": False,
+    "installable": True,
+}
