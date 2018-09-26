44d7230ffee30e7196072d3f2234a2e29abd4e22
GabbasovDinar <gabbasov@it-projects.info>
2018-09-26 21:08:46 +0500

    :memo: update license

--- pos_order_print_check/static/src/js/models.js
+++ pos_order_print_check/static/src/js/models.js
@@ -1,3 +1,5 @@
+/* Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+ * License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html). */
 odoo.define('pos_order_print_check.models', function (require) {
     "use strict";
 
