e15424f11c65bc891e0ea6e8316fcf106e21b2af
GabbasovDinar <gabbasov@it-projects.info>
2018-10-22 21:46:59 +0500

    :arrow_up::one::two: pos_order_printer_product

--- pos_order_printer_product/__manifest__.py
+++ pos_order_printer_product/__manifest__.py
@@ -4,7 +4,7 @@
     "category": "Point of Sale",
     # "live_test_URL": "",
     "images": ['images/pos_order_printer_product_main.png'],
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "application": False,
 
     "author": "IT-Projects LLC, Dinar Gabbasov",
@@ -26,7 +26,6 @@
     "qweb": [
     ],
     "demo": [
-        "data/pos_order_printer_product_demo.xml",
     ],
 
     "post_load": None,
@@ -34,5 +33,5 @@
     "post_init_hook": None,
 
     "auto_install": False,
-    "installable": False,
+    "installable": True,
 }
