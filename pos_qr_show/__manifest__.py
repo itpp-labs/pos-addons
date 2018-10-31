2015d24a93b5ed302f2bc65eb7e1067453dd0952
GabbasovDinar <gabbasov@it-projects.info>
2018-10-31 15:13:55 +0500

    :arrow_up::one::two: pos_qr_show

--- pos_qr_show/__manifest__.py
+++ pos_qr_show/__manifest__.py
@@ -6,12 +6,12 @@
     "category": "Hidden",
     # "live_test_url": "",
     "images": [],
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "application": False,
 
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "support": "apps@it-projects.info",
-    "website": "https://it-projects.info/team/yelizariev",
+    "website": "https://apps.odoo.com/apps/modules/12.0/pos_qr_show/",
     "license": "LGPL-3",
     "price": 40.00,
     "currency": "EUR",
@@ -35,5 +35,5 @@
     "uninstall_hook": None,
 
     "auto_install": False,
-    "installable": False,
+    "installable": True,
 }
