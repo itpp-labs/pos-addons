c6eed841f60940167bde96b5ff94e96b940290a4
GabbasovDinar <gabbasov@it-projects.info>
2018-11-09 15:23:42 +0500

    :shield: pos_order_cancel_restaurant

--- pos_order_cancel_restaurant/static/src/js/tour.js
+++ pos_order_cancel_restaurant/static/src/js/tour.js
@@ -2,20 +2,17 @@ odoo.define('pos_order_cancel_restaurant.tour', function(require) {
     "use strict";
 
     require("pos_order_cancel.tour");
-    var core = require('web.core');
     var tour = require('web_tour.tour');
     var steps = tour.tours.pos_order_cancel_tour.steps;
 
-    var _t = core._t;
-
     for (var position = 0; position < steps.length; position++) {
-        if (steps[position].trigger === ".product-list .product") {
+        if (steps[position].trigger === ".numpad-backspace") {
             steps.splice(
                 position,
                 0,
                 {
-                    trigger: ".tables .table",
-                    content: _t("<p>Click <b>table</b></p>"),
+                    trigger: ".order-submit",
+                    content: "Send the order to kitchen",
                     position: "bottom"
                 }
             );
--- pos_order_cancel_restaurant/tests/test_default.py
+++ pos_order_cancel_restaurant/tests/test_default.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 import odoo.tests
+from odoo.api import Environment
 
 
-@odoo.tests.common.at_install(False)
+# tests is not work when pos_category_multi and pos_order_cancel_restaurant was installed,
+# pos_category_multi replaces the pos_categ_id field to pos_category_ids
+# to use at_install
+
+@odoo.tests.common.at_install(True)
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
 
     def test_01_pos_is_loaded(self):
+        # see more https://odoo-development.readthedocs.io/en/latest/dev/tests/js.html#phantom-js-python-tests
+        env = Environment(self.registry.test_cr, self.uid, {})
+
+        main_pos_config = env.ref('point_of_sale.pos_config_main')
+
+        main_pos_config.write({
+            'save_canceled_kitchen_orders_only': True,
+        })
+
         self.phantom_js(
             '/web',
 
