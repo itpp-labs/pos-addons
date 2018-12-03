83da78ba90dbcffa408df526b808fac37f31c22e
GabbasovDinar <gabbasov@it-projects.info>
2018-12-03 19:07:11 +0500

    :heart_eyes: show the order ticket if not exist receipt for the in reprint mode

--- pos_orders_history_reprint/__manifest__.py
+++ pos_orders_history_reprint/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Point of Sale",
     "live_test_url": "http://apps.it-projects.info/shop/product/pos-orders-reprint?version=10.0",
     "images": ["images/pos_orders_reprint_main.png"],
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "application": False,
 
     "author": "IT-Projects LLC, Artyom Losev, Dinar Gabbasov",
