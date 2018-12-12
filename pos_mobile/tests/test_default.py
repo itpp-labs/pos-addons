457f62e1ccaba44dccf4d56a35fb3f22674dee48
GabbasovDinar <gabbasov@it-projects.info>
2018-12-12 12:22:56 +0500

    :shield: pos_mobile

--- pos_mobile/tests/test_default.py
+++ pos_mobile/tests/test_default.py
@@ -1,5 +1,4 @@
 import odoo.tests
-from odoo.api import Environment
 
 
 # tests is not work when pos_mobile and pos_restaurant was installed,
@@ -11,7 +10,7 @@ class TestUi(odoo.tests.HttpCase):
 
     def test_01_pos_is_loaded(self):
         # see more https://odoo-development.readthedocs.io/en/latest/dev/tests/js.html#phantom-js-python-tests
-        env = Environment(self.registry.test_cr, self.uid, {})
+        env = self.env
 
         # get exist pos_config
         main_pos_config = env.ref('point_of_sale.pos_config_main')
