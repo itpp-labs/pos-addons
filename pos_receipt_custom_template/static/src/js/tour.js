c0a10deaedcaca502c430412369ab2454197b14d
GabbasovDinar <gabbasov@it-projects.info>
2018-12-12 12:22:18 +0500

    :shield: pos_receipt_custom_template

--- pos_receipt_custom_template/static/src/js/tour.js
+++ pos_receipt_custom_template/static/src/js/tour.js
@@ -80,6 +80,7 @@ odoo.define('pos_receipt_custom_template.tour', function(require) {
     var steps = [{
         trigger: '.o_main_content:has(.loader:hidden)',
         content: 'waiting for loading to finish',
+        timeout: 20000,
         run: function () {
             // it's a check
         },
@@ -91,11 +92,11 @@ odoo.define('pos_receipt_custom_template.tour', function(require) {
         position: "bottom"
     });
 
-    steps = steps.concat(add_product_to_order('Peaches'));
+    steps = steps.concat(add_product_to_order('LED Lamp'));
 
     steps = steps.concat(goto_payment_screen_and_select_payment_method());
 
-    steps = steps.concat(generate_payment_screen_keypad_steps("10"));
+    steps = steps.concat(generate_payment_screen_keypad_steps("0.9"));
 
     steps = steps.concat(finish_order());
 
@@ -107,7 +108,7 @@ odoo.define('pos_receipt_custom_template.tour', function(require) {
         content: "confirm closing the frontend",
     }, {
         content: "wait until backend is opened",
-        trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"], .oe_menu_toggler[data-menu-xmlid="point_of_sale.menu_point_root"]',
+        trigger: '.o_pos_kanban button.oe_kanban_action_button',
         run: function () {
             // no need to click on trigger
         },
