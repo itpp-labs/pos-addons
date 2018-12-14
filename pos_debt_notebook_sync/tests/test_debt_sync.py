75d2cf8699e0d995e81c03a7a216c6560ebe135e
GabbasovDinar <gabbasov@it-projects.info>
2018-12-14 15:54:27 +0500

    :shield: pos_debt_notebook_sync

--- pos_debt_notebook_sync/tests/test_debt_sync.py
+++ pos_debt_notebook_sync/tests/test_debt_sync.py
@@ -9,13 +9,6 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_pos_debt(self):
-        # needed because tests are run before the module is marked as
-        # installed. In js web will only load qweb coming from modules
-        # that are returned by the backend in module_boot. Without
-        # this you end up with js, css but no qweb.
-        env = self.env
-        env['ir.module.module'].search([('name', '=', 'pos_debt_notebook_sync')], limit=1).state = 'installed'
-
         # without a delay there might be problems caused by a not yet loaded button's action
         self.phantom_js("/web",
                         "odoo.__DEBUG__.services['web_tour.tour'].run('tour_pos_debt_notebook', 1000)",
