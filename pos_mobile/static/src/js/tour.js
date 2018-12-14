c71166d5a77040db86c5c4c6abd591363c066cd3
GabbasovDinar <gabbasov@it-projects.info>
2018-12-14 18:22:34 +0500

    :shield: passing tests

--- pos_mobile/static/src/js/tour.js
+++ pos_mobile/static/src/js/tour.js
@@ -32,7 +32,7 @@ odoo.define('pos_mobile.tour', function(require) {
             steps = steps.concat(cashier_select());
         }
         steps = steps.concat([{
-            trigger: '.paymentmethod:contains("Cash")',
+            trigger: '.paymentmethod:contains("Cash (USD)")',
             content: "pay with cash",
         }]);
         return steps;
