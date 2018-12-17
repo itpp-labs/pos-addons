5b4bec87c936ff840990151f19ce3bdeb5b05740
GabbasovDinar <gabbasov@it-projects.info>
2018-12-17 15:25:38 +0500

    :ambulance: error of adding a product after pressing on the Enter in the search bar

--- pos_category_multi/__manifest__.py
+++ pos_category_multi/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": """Specify as many categories for a product as you need""",
     "category": "Point of Sale",
     "images": [],
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
 
     "author": "IT-Projects LLC, Pavel Romanchenko",
     "support": "pos@it-projects.info",
