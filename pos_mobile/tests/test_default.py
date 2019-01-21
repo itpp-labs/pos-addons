6c00c844c7b035674760c8fa3054013698f36108
GabbasovDinar <gabbasov@it-projects.info>
2019-01-21 14:06:43 +0500

    :shield: pos_mobile

--- pos_mobile/tests/test_default.py
+++ pos_mobile/tests/test_default.py
@@ -4,18 +4,19 @@ import odoo.tests
 # tests is not work when pos_mobile and pos_restaurant was installed,
 # but pos_mobile_restaurant was not installed so need
 # to use post_install
-@odoo.tests.common.at_install(False)
-@odoo.tests.common.post_install(True)
+@odoo.tests.tagged('post_install', '-at_install')
 class TestUi(odoo.tests.HttpCase):
 
     def test_01_pos_is_loaded(self):
         # see more https://odoo-development.readthedocs.io/en/latest/dev/tests/js.html#phantom-js-python-tests
-        env = self.env
+        env = self.env(user=self.env.ref('base.user_admin'))
 
         # get exist pos_config
         main_pos_config = env.ref('point_of_sale.pos_config_main')
         # create new session and open it
         main_pos_config.open_session_cb()
+
+        env['ir.module.module'].search([('name', '=', 'pos_mobile')], limit=1).state = 'installed'
 
         self.phantom_js(
             '/pos/web?m=1',
