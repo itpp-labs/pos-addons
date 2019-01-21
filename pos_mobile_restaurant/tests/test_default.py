64e9cc2e6b7976b8ff99ebdb425084fab570f7b2
GabbasovDinar <gabbasov@it-projects.info>
2019-01-21 14:44:31 +0500

    :shield: pos_mobile_restaurant

--- pos_mobile_restaurant/tests/test_default.py
+++ pos_mobile_restaurant/tests/test_default.py
@@ -1,20 +1,20 @@
 import odoo.tests
 
 
-@odoo.tests.common.at_install(True)
-@odoo.tests.common.post_install(True)
+@odoo.tests.tagged('post_install', 'at_install')
 class TestUi(odoo.tests.HttpCase):
 
     def test_01_pos_is_loaded(self):
+
         # see more https://odoo-development.readthedocs.io/en/latest/dev/tests/js.html#phantom-js-python-tests
-        env = self.env
+        env = self.env(user=self.env.ref('base.user_admin'))
 
         # get exist pos_config
-        main_pos_config = env.ref('point_of_sale.pos_config_main')
+        bar_pos_config = env.ref('pos_restaurant.pos_config_restaurant')
         # create new session and open it
-        main_pos_config.open_session_cb()
+        bar_pos_config.open_session_cb()
 
-        env['ir.module.module'].search([('name', '=', 'pos_mobile')], limit=1).state = 'installed'
+        env['ir.module.module'].search([('name', '=', 'pos_mobile_restaurant')], limit=1).state = 'installed'
 
         self.phantom_js(
             '/pos/web?m=1',
