07962b83c45cf802a49cfb8e0b3b6118dc070087
ILMIR <karamov@it-projects.info>
2019-02-13 17:19:08 +0500

    :book: pos_debt_notebook_sync: live_test_url version

--- pos_debt_notebook_sync/__manifest__.py
+++ pos_debt_notebook_sync/__manifest__.py
@@ -6,7 +6,7 @@
     "name": """Internal Credit System""",
     "summary": """Credit payment system for festivals, food courts, etc.""",
     "category": "Point of Sale",
-    'live_test_url': 'http://apps.it-projects.info/shop/product/internal-credit-system?version=10.0',
+    'live_test_url': 'http://apps.it-projects.info/shop/product/internal-credit-system?version=11.0',
     "images": ['images/credit.png'],
     "version": "11.0.1.1.2",
     "application": False,
