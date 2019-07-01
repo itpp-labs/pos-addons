fb8040fdedaa717cee84234f0190b1e0085d12ec
KolushovAlexandr <kolushov@it-projects.info>
2019-07-01 15:07:13 +0500

    :zap: new customization options

--- pos_product_available_negative/models.py
+++ pos_product_available_negative/models.py
@@ -1,7 +1,7 @@
 # Copyright 2016 Stanislav Krotov <https://it-projects.info/team/ufaks>
 # Copyright 2016 manawi <https://github.com/manawi>
+# Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
 from odoo import models, fields, api
 
 
@@ -15,6 +15,14 @@ class PosConfig(models.Model):
     negative_order_group_id = fields.Many2one(
         'res.groups', string='Negative Order Group', default=_default_negative_stock_user,
         help='Group allows to sell products which are out of a stock.')
+    negative_order_manager_permission = fields.Boolean(
+        "Managers Permission",
+        help="Ask Managers Permission to proceed order with negative stock products",
+        default=True)
+    negative_order_warning = fields.Boolean(
+        "Show Warning",
+        help="Show Warning on adding out of stock products",
+        default=False)
 
 
 class PosOrder(models.Model):
@@ -27,5 +35,5 @@ class PosOrder(models.Model):
     @api.model
     def _order_fields(self, ui_order):
         res = super(PosOrder, self)._order_fields(ui_order)
-        res['negative_stock_user_id'] = ui_order['negative_stock_user_id']
+        res['negative_stock_user_id'] = ui_order.get('negative_stock_user_id', False)
         return res
