cc05b199cbca57cb5a59749b876a44aaf6c0f921
Vildan <vildan.safin.99@gmail.com>
2019-07-08 16:16:58 +0500

    Made some changes in __manifest__.py

--- pos_chat/__manifest__.py
+++ pos_chat/__manifest__.py
@@ -1,44 +1,44 @@
 # -*- coding: utf-8 -*-
-{
-    'version': "12.0",
-    'name': "Pos chat",
-    'summary': "Simple chat for simple users",
-    'category': "Point of Sale",
+.{
+    "version": "12.0.0.0.0",
+    "name": """Pos_chat""",
+    "summary": """Simple chat for simple users""",
+    "category": "Point of Sale",
     # 'live_test_url': "",
-    'images': [],
-    'application': False,
+    "images": [],
+    "application": False,
 
-    'author': "IT-Projects LLC, Safin Vildan",
-    'website': "https://it-projects.info/team/yelizariev",
-    'license': "LGPL-3",
+    "author": "IT-Projects LLC, Safin Vildan",
+    "website": "https://it-projects.info/team/yelizariev",
+    "license": "LGPL-3",
     # 'price: 250.00,
     # 'currency: "EUR",g
 
-    'depends': [],
-    'external_dependencies': {"python": [], "bin": []},
+    "depends": [],
+    "external_dependencies": {"python": [], "bin": []},
 
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'data/data.xml',
-        'views/views.xml',
-        'views/templates.xml',
+        "data/data.xml",
+        "views/views.xml",
+        "views/templates.xml",
     ],
-    'qweb': [
+    "qweb": [
         # 'static/src/xml/*.xml',
     ],
-    'demo': [
-        'demo/demo.xml'
+    "demo": [
+        "demo/demo.xml"
     ],
 
-    'post_load': None,
-    'pre_init_hook': None,
-    'post_init_hook': None,
-    'uninstall_hook': None,
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
 
-    'auto_install': False,
-    'installable': True,
+    "auto_install": False,
+    "installable": True,
 
-    'description': """
-None
-"""
+    "description": """Mudule allows users to chat with each other"""
 }
+
+
