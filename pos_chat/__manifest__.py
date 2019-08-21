905ba676be2016c26560e9f0f9215798b04019dd
Vildan <vildan.safin.99@gmail.com>
2019-08-21 16:31:45 +0500

    addded pos_longpooling dependense(one more time without ?)

--- pos_chat/__manifest__.py
+++ pos_chat/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['point_of_sale', 'pos_longpolling?'],
+    'depends': ['point_of_sale', 'pos_longpolling'],
 
     # always loaded
     'data': [
