14ed935aa47b396a8ccc56a00f4323396ed4e13e
KolushovAlexandr <kolushov@it-projects.info>
2019-08-27 13:13:50 +0500

    :rainbow: removed excessive "require"ments in js

--- pos_multi_session/static/src/js/pos_multi_session.js
+++ pos_multi_session/static/src/js/pos_multi_session.js
@@ -18,9 +18,7 @@ odoo.define('pos_multi_session', function(require){
     var chrome = require('point_of_sale.chrome');
     var longpolling = require('pos_longpolling');
     var rpc = require('web.rpc');
-    var PosBaseWidget = require('point_of_sale.BaseWidget');
     var gui = require('point_of_sale.gui');
-    var framework = require('web.framework');
     var posDB = require('point_of_sale.DB');
 
 
