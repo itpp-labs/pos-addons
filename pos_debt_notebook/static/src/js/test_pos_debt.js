7b918cf649a8a7148f4112d9c3c33871cb683c37
fedoranvar <kildebekov@it-projects.info>
2019-10-01 10:57:42 +0500

    :shield: unit_tour-tests

--- pos_debt_notebook/static/src/js/test_pos_debt.js
+++ pos_debt_notebook/static/src/js/test_pos_debt.js
@@ -1,10 +1,13 @@
-odoo.define('pos_debt_notebook.tour', function (require) {
+/* Copyright 2019 Anvar Kildebekov <https://it-projects.info/team/fedoranvar>
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html). */
+ odoo.define('pos_debt_notebook.tour', function (require) {
     "use strict";
 
     var tour = require("web_tour.tour");
     var core = require('web.core');
     var _t = core._t;
 
+    /* -----Actions----- */
     function open_pos_neworder() {
         return [{
             trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"], .oe_menu_toggler[data-menu-xmlid="point_of_sale.menu_point_root"]',
@@ -19,12 +22,14 @@ odoo.define('pos_debt_notebook.tour', function (require) {
             trigger: '.table:not(.oe_invisible .neworder-button), .order-button.selected',
             position: "bottom",
             timeout: 20000,
-        }, {
-            content: 'waiting for loading to finish',
+        }];
+    }
+    function create_new_order() {
+        return [{
+            content: 'Create new order',
             trigger: '.order-button.neworder-button',
         }];
     }
-
     function add_product_to_order(product_name) {
         return [{
             content: 'buy ' + product_name,
@@ -34,13 +39,19 @@ odoo.define('pos_debt_notebook.tour', function (require) {
             trigger: '.order .product-name:contains("' + product_name + '")',
         }];
     }
-
+    function switch_table() {
+        return [{
+            content: "Switch to table or make dummy action",
+            trigger: '.table:not(.oe_invisible .neworder-button), .order-button.selected',
+            position: "bottom",
+        }];
+    }
     function set_customer(name) {
         return [{
-            trigger: '.button.set-customer',
+            trigger: '.button.js_set_customer',
             content: _t("Open the customer screen"),
         }, {
-            trigger: 'td:contains("' + name + '")',
+            trigger: 'td:contains(' + name + ')',
             content: _t("Click the customer"),
         }, {
             extra_trigger: '.button.next.highlight:contains("Set Customer")',
@@ -48,8 +59,16 @@ odoo.define('pos_debt_notebook.tour', function (require) {
             content: 'Set Customer',
         }];
     }
-
-    function debt_method_paying(pay_method) {
+    function deselect_customer() {
+        return [{
+            trigger: '.button.js_set_customer',
+            content: _t("Open the customer screen"),
+        }, {
+            trigger: '.button:contains("Deselect")',
+            content: _t("Deselect the customer"),
+        }];
+    }
+    function click_to_payment() {
         return [{
             content: "Make a dummy action",
             trigger: '.order-button.selected',
@@ -59,27 +78,197 @@ odoo.define('pos_debt_notebook.tour', function (require) {
         }, {
             content: "Choose Administrator like a cashier or make a dummy action",
             trigger: '.modal-dialog.cashier:not(.oe_hidden) .cashier .selection-item:contains("Administrator"), .payment-screen:not(.oe_hidden) h1:contains("Payment")',
-        }, {
+        }];
+    }
+    function debt_method_paying(pay_method) {
+        return [{
             extra_trigger: '.button.paymentmethod:contains("' + pay_method +'")',
             trigger: '.button.paymentmethod:contains("' + pay_method +'")',
             content: _t("Click the payment method"),
-        }, {
+        }];
+    }
+    function validate_order() {
+        return [{
             extra_trigger: '.button.next.highlight:contains("Validate")',
             trigger: '.button.next.highlight:contains("Validate")',
             content: 'Validate payment',
-        }, {
-            extra_trigger: '.pos-sale-ticket',
-            trigger: '.button.next.highlight:contains("Next Order")',
-            content: 'Check proceeded validation',
         }];
+    }
+    function close_error_modal_dialog(message) {
+        return [{
+            trigger: '.popup>.body:contains("' + message +'")',
+            content: _t("check error-message"),
+        },{
+            trigger: '.modal-dialog .button:contains("Ok")',
+            content: _t("Close alert-dialog"),
+        }];
+    }
+    function click_numpad_input(char) {
+        return [{
+            trigger: '.input-button:contains("' + char +'")',
+            content: _t("Click input on numpad"),
+        }];
+    }
+    function next_order(){
+        return [{
+            trigger: '.button.next',
+            content: _t("Next order"),
+        }];
+    }
+    function set_numpad_value(value) {
+        var steps = [];
+        for (var i = 0; i < value.length; i++) {
+            steps = steps.concat(click_numpad_input(value.charAt(i)));
+       }
+       return steps;
+    }
+    function remove_paymentline(){
+        return [{
+            trigger: '.paymentline>.delete-button',
+            content: _t("delete selected payment-line"),
+        }];
+    }
+    function click_autopay_button(){
+        return [{
+            trigger: '.autopay',
+            content: _t("click autopay-button"),
+        }];
+    }
+    /* -----Scenarios----- */
+    // Initial Test "Pay with all credit-journal"
+    function initial_scene(steps, client) {
+        return steps.concat(
+            create_new_order(),
+            add_product_to_order('Lemon'),
+            click_to_payment(),
+            set_customer(client),
+            debt_method_paying('Credits (USD)'),
+            set_numpad_value("1"),
+            debt_method_paying('Credits (Fruits & Vegetables only) (USD)'),
+            set_numpad_value("1"),
+            debt_method_paying('Credits (via discounts) (USD)'),
+            validate_order(),
+            next_order(),
+            switch_table()
+        );
+    }
+    // Error: "You cannot use Debt payment. Select customer first."
+    function scene_1(steps, client, err_msg) {
+        return steps.concat(
+            create_new_order(),
+            add_product_to_order("Miscellaneous"),
+            click_to_payment(),
+            set_customer(client),
+            debt_method_paying('Credits (USD)'),
+            deselect_customer(),
+            validate_order(),
+            close_error_modal_dialog(err_msg)
+        );
+    }
+    // Error:  "You cannot sell products on credit to the customer because his max debt value will be exceeded."
+    function scene_2(steps, client, err_msg) {
+        return steps.concat(
+            create_new_order(),
+            add_product_to_order("Miscellaneous"),
+            click_to_payment(),
+            set_customer(client),
+            debt_method_paying('Credits (USD)'),
+            set_numpad_value("10000"),
+            validate_order(),
+            close_error_modal_dialog(err_msg)
+        );
+    }
+    // Scenario: Increase credit amount
+    function scene_3(steps, client) {
+        return steps.concat(
+            create_new_order(),
+            add_product_to_order("Pay Debt"),
+            click_to_payment(),
+            set_customer(client),
+            debt_method_paying('Cash (USD)'),
+            set_numpad_value("4"),
+            debt_method_paying('Credits (USD)'),
+            validate_order(),
+            next_order(),
+            switch_table()
+        );
+    }
+    // Scenario: Transfer money from Credit to Credit (Fruit&Vegetables only)
+    function scene_4(steps, client) {
+        return steps.concat(
+            create_new_order(),
+            add_product_to_order("Pay Debt"),
+            click_to_payment(),
+            set_customer(client),
+            debt_method_paying('Credits (USD)'),
+            set_numpad_value("2"),
+            debt_method_paying('Credits (Fruits & Vegetables only) (USD)'),
+            validate_order(),
+            next_order(),
+            switch_table()
+        );
+    }
+    // Error: "Please enter the exact or lower debt amount than the cost of the order"
+    function scene_5(steps, client, err_msg) {
+        return steps.concat(
+            create_new_order(),
+            add_product_to_order("Pay Debt"),
+            click_to_payment(),
+            set_customer(client),
+            debt_method_paying('Credits (Fruits & Vegetables only) (USD)'),
+            set_numpad_value("1"),
+            debt_method_paying('Credits (USD)'),
+            validate_order(),
+            close_error_modal_dialog(err_msg)
+        );
+    }
+    // Error: "You may only buy Fruits and Vegetables with Credits (Fruits & Vegetables only) (USD)"
+    function scene_6(steps, client, err_msg) {
+        return steps.concat(
+            create_new_order(),
+            add_product_to_order("Miscellaneous"),
+            click_to_payment(),
+            set_customer(client),
+            remove_paymentline(),
+            debt_method_paying('Credits (Fruits & Vegetables only) (USD)'),
+            set_numpad_value("1"),
+            validate_order(),
+            close_error_modal_dialog(err_msg)
+        );
+    }
+    // Scenario: Proceed order with autopay-button (bottom-left corner)
+    function scene_7(steps, client) {
+        return steps.concat(
+            create_new_order(),
+            add_product_to_order("Miscellaneous"),
+            click_to_payment(),
+            set_customer(client),
+            remove_paymentline(),
+            debt_method_paying('Credits (via discounts) (USD)'),
+            set_numpad_value("1"),
+            click_autopay_button(),
+            click_autopay_button()
+        );
     }
 
     var steps = [];
+    var client = "Agrolait";
+    var err_msgs = [
+        "You cannot use Debt payment. Select customer first.",
+        "You cannot sell products on credit journal Credits (USD) to the customer because its max debt value will be exceeded.",
+        "Please enter the exact or lower debt amount than the cost of the order.",
+        "You may only buy Fruits and Vegetables with Credits (Fruits & Vegetables only) (USD)",
+    ];
+
     steps = steps.concat(open_pos_neworder());
-    steps = steps.concat(add_product_to_order('Miscellaneous'));
-    steps = steps.concat(set_customer('Agrolait'));
-    steps = steps.concat(debt_method_paying('Credits (USD)'));
+    steps = initial_scene(steps, client);
+    steps = scene_1(steps, client, err_msgs[0]);
+    steps = scene_2(steps, client, err_msgs[1]);
+    steps = scene_3(steps, client);
+    steps = scene_4(steps, client);
+    steps = scene_5(steps, client, err_msgs[2]);
+    steps = scene_6(steps, client, err_msgs[3]);
+    steps = scene_7(steps, client);
 
     tour.register('tour_pos_debt_notebook', { test: true, url: '/web' }, steps);
-
 });
--- pos_debt_notebook/tests/test_pos_debt.py
+++ pos_debt_notebook/tests/test_pos_debt.py
@@ -1,3 +1,5 @@
+# Copyright 2019 Anvar Kildebekov <https://it-projects.info/team/fedoranvar>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 import odoo.tests
 from odoo.api import Environment
 
@@ -6,7 +8,7 @@ from odoo.api import Environment
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
 
-    def test_pos_debt(self):
+    def common_test(self, test_login):
         # needed because tests are run before the module is marked as
         # installed. In js web will only load qweb coming from modules
         # that are returned by the backend in module_boot. Without
@@ -15,10 +17,26 @@ class TestUi(odoo.tests.HttpCase):
         env = Environment(cr, self.uid, {})
         env['ir.module.module'].search([('name', '=', 'pos_debt_notebook')], limit=1).state = 'installed'
         cr.release()
-
         # without a delay there might be problems on the steps whilst opening a POS
         # caused by a not yet loaded button's action
+        # Step 1: Tour Test
+        # import wdb;wdb.set_trace()
+        self.env['product.product'].search([("name", "=", "Lemon")]).list_price = 3
+        self.env['product.product'].search([("name", "=", "Miscellaneous")]).list_price = 1
         self.phantom_js("/web",
-                        "odoo.__DEBUG__.services['web_tour.tour'].run('tour_pos_debt_notebook', 1000)",
+                        "odoo.__DEBUG__.services['web_tour.tour'].run('tour_pos_debt_notebook', 500)",
                         "odoo.__DEBUG__.services['web_tour.tour'].tours.tour_pos_debt_notebook.ready",
-                        login="admin", timeout=140)
+                        login=test_login, timeout=200)
+        # Step 2: Check debt-balance
+        test_partner = env['res.partner'].search([('name', '=', 'Agrolait')])
+        test_journal = [
+            env['account.journal'].search([('name', '=', 'Credits')]),
+            env['account.journal'].search([('name', '=', 'Credits (Fruits & Vegetables only)')])
+        ]
+        #  check for 'Credits' balance
+        self.assertEqual(test_partner.debt_history()[test_partner.id]['debts'][test_journal[0].id]['balance'], 1.0)
+        #  check for 'Credits (Fruits&Vegetables)' balance
+        self.assertEqual(test_partner.debt_history()[test_partner.id]['debts'][test_journal[1].id]['balance'], 1.0)
+
+    def test_01_pos_debt(self):
+        self.common_test("admin")
