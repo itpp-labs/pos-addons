e2d8827abaf15b12b9c91894b91e50c63ceb7ce5
Artur Nigmatullin <nigmatullin@it-projects.info>
2019-10-19 20:52:51 +0500

    :euro:

--- pos_qr_payments/__manifest__.py
+++ pos_qr_payments/__manifest__.py
@@ -13,7 +13,7 @@
     "support": "pos@it-projects.info",
     "website": "https://apps.odoo.com/apps/modules/12.0/pos_qr_payments/",
     "license": "LGPL-3",
-    "price": 20.00,
+    "price": 10.00,
     "currency": "EUR",
 
     "depends": [
