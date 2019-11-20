b9e07565656ef01d1fbd3a51f1ece5b04df26683
Eugene Molotov <molotov@it-projects.info>
2019-11-20 11:19:00 +0000

    :ambulance: pos_qr_scan: fixed ReferenceError: gCtx is not defined

--- pos_qr_scan/doc/changelog.rst
+++ pos_qr_scan/doc/changelog.rst
@@ -1,3 +1,8 @@
+`1.0.2`
+-------
+
+- **Fix:** - Scanner didn't work due to "ReferenceError: gCtx is not defined"
+
 `1.0.1`
 -------
 
--- pos_qr_scan/static/src/js/qr_scan.js
+++ pos_qr_scan/static/src/js/qr_scan.js
@@ -171,7 +171,7 @@ odoo.define("pos_qr_scan", function (require) {
             if (this.gUM) {
                 var self = this;
                 try {
-                    gCtx.drawImage(this.video_element, 0, 0);
+                    this.gCtx.drawImage(this.video_element, 0, 0);
                     try {
                         qrcode.decode();
                     } catch (e) {
@@ -196,6 +196,7 @@ odoo.define("pos_qr_scan", function (require) {
             gCanvas.height = h;
             var gCtx = gCanvas.getContext("2d");
             gCtx.clearRect(0, 0, w, h);
+            this.gCtx = gCtx;
         },
     });
 
