dbd3e7e9e163859eb6c97704d5818dc45fafdbdd
Eugene Molotov <molotov@it-projects.info>
2019-11-20 11:19:00 +0000

    :ambulance: pos_qr_scan: fixed ReferenceError: gCtx is not defined

--- pos_qr_scan/__manifest__.py
+++ pos_qr_scan/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """Scans QR codes via device's camera""",
     "category": "Point of Sale",
     "images": ["images/main.png"],
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "application": False,
     "author": "IT-Projects LLC, KolushovAlexandr",
     "support": "help@itpp.dev",
