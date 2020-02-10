8fc650b887d2d0de8be21d8323e51169ff1663d7
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-10 09:54:24 +0000

    :green_heart: pos_invoice_pay/

--- pos_invoice_pay/tests/test_pos_invoice.py
+++ pos_invoice_pay/tests/test_pos_invoice.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# pylint: disable=method-required-super
 
 from mock import MagicMock
 
