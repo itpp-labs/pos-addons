c46099f525c1ac89bfc04759fe3829b07064a28f
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-11 12:10:35 +0000

    :green_heart:  M pos_orders_history/static/src/js/db.js  M pos_orders_history/static/src/js/screens.js

--- pos_orders_history/static/src/js/db.js
+++ pos_orders_history/static/src/js/db.js
@@ -1,6 +1,7 @@
 /* Copyright 2017-2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
  * Copyright 2018 Artem Losev
  * License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html). */
+/* eslint-disable no-useless-escape */
 odoo.define("pos_orders_history.db", function(require) {
     "use strict";
     var PosDB = require("point_of_sale.DB");
