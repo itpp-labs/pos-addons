48cd9f3b2491e6125ff494dc40950c95c263efa4
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-11 12:21:36 +0000

    :green_heart:  M pos_orderline_absolute_discount/static/src/js/models.js  M pos_orderline_absolute_discount/static/src/js/screens.js

--- pos_orderline_absolute_discount/static/src/js/models.js
+++ pos_orderline_absolute_discount/static/src/js/models.js
@@ -1,11 +1,12 @@
 odoo.define("pos_absolute_discount.models", function(require) {
+    "use strict";
     var models = require("point_of_sale.models");
     var utils = require("web.utils");
     var core = require("web.core");
 
     var _t = core._t;
     var round_pr = utils.round_precision;
-    var round_di = utils.round_decimals;
+    // Var round_di = utils.round_decimals;
 
     var _super_orderline = models.Orderline.prototype;
     models.Orderline = models.Orderline.extend({
