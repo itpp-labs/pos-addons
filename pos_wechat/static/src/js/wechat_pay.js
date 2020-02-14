7e6ad8153a428312e14146015f6bde1678b46dad
Vildan Safin <safin@it-projects.info>
2020-02-14 21:21:14 +0500

    :rainbow: pre-commit: pos_wechat cleanup
    Signed-off-by: Vildan Safin <safin@it-projects.info>

--- pos_wechat/static/src/js/wechat_pay.js
+++ pos_wechat/static/src/js/wechat_pay.js
@@ -9,114 +9,9 @@ odoo.define("pos_wechat", function(require) {
     var rpc = require("web.rpc");
     var core = require("web.core");
     var models = require("point_of_sale.models");
-
-    var _t = core._t;
+    var Backbone = window.Backbone;
 
     models.load_fields("account.journal", ["wechat"]);
-
-    var PosModelSuper = models.PosModel;
-    models.PosModel = models.PosModel.extend({
-        initialize: function() {
-            var self = this;
-            PosModelSuper.prototype.initialize.apply(this, arguments);
-            this.wechat = new Wechat(this);
-
-            this.bus.add_channel_callback("wechat", this.on_wechat, this);
-            this.ready.then(function() {
-                // Take out wechat micropay cashregister from cashregisters to avoid
-                // rendering in payment screent
-                self.micropay_journal = self.hide_cashregister(function(r) {
-                    return r.wechat == "micropay";
-                });
-            });
-        },
-        scan_product: function(parsed_code) {
-            // TODO: do we need to make this optional?
-            var value = parsed_code.code;
-            if (this.wechat.check_auth_code(value)) {
-                this.wechat.process_qr(value);
-                return true;
-            }
-            return PosModelSuper.prototype.scan_product.apply(this, arguments);
-        },
-        on_wechat: function(msg) {
-            this.add_qr_payment(
-                msg.order_ref,
-                msg.journal_id,
-                msg.total_fee / 100.0,
-                {
-                    micropay_id: msg.micropay_id,
-                },
-                true // Auto validate payment
-            );
-        },
-        wechat_qr_payment: function(order, creg) {
-            /* Send request asynchronously */
-            var self = this;
-
-            var pos = this;
-            var terminal_ref = "POS/" + pos.config.name;
-            var pos_id = pos.config.id;
-
-            var lines = order.orderlines.map(function(r) {
-                return {
-                    quantity: 1, // Always use 1 because quantity is taken into account in price field
-                    quantity_full: r.get_quantity(),
-                    price: r.get_price_with_tax(),
-                    product_id: r.get_product().id,
-                };
-            });
-
-            // Send without repeating on failure
-            return rpc
-                .query({
-                    model: "wechat.order",
-                    method: "create_qr",
-                    kwargs: {
-                        lines: lines,
-                        order_ref: order.uid,
-                        pay_amount: order.get_due(),
-                        terminal_ref: terminal_ref,
-                        pos_id: pos_id,
-                        journal_id: creg.journal.id,
-                    },
-                })
-                .then(function(data) {
-                    if (data.code_url) {
-                        self.on_payment_qr(order, data.code_url);
-                    } else if (data.error) {
-                        self.show_warning(data.error);
-                    } else {
-                        self.show_warning("Unknown error");
-                    }
-                });
-        },
-    });
-
-    var OrderSuper = models.Order;
-    models.Order = models.Order.extend({
-        add_paymentline: function(cashregister) {
-            if (cashregister.journal.wechat == "native") {
-                this.pos.wechat_qr_payment(this, cashregister);
-                return;
-            }
-            return OrderSuper.prototype.add_paymentline.apply(this, arguments);
-        },
-    });
-
-    var PaymentlineSuper = models.Paymentline;
-    models.Paymentline = models.Paymentline.extend({
-        initialize: function(attributes, options) {
-            PaymentlineSuper.prototype.initialize.apply(this, arguments);
-            this.micropay_id = options.micropay_id;
-        },
-        // TODO: do we need to extend init_from_JSON too ?
-        export_as_JSON: function() {
-            var res = PaymentlineSuper.prototype.export_as_JSON.apply(this, arguments);
-            res.micropay_id = this.micropay_id;
-            return res;
-        },
-    });
 
     var Wechat = Backbone.Model.extend({
         initialize: function(pos) {
@@ -194,6 +89,112 @@ odoo.define("pos_wechat", function(require) {
                 }
                 self.pos.show_warning();
             });
+        },
+    });
+
+    var PosModelSuper = models.PosModel;
+    models.PosModel = models.PosModel.extend({
+        initialize: function() {
+            var self = this;
+            PosModelSuper.prototype.initialize.apply(this, arguments);
+            this.wechat = new Wechat(this);
+
+            this.bus.add_channel_callback("wechat", this.on_wechat, this);
+            this.ready.then(function() {
+                // Take out wechat micropay cashregister from cashregisters to avoid
+                // rendering in payment screent
+                self.micropay_journal = self.hide_cashregister(function(r) {
+                    return r.wechat === "micropay";
+                });
+            });
+        },
+        scan_product: function(parsed_code) {
+            // TODO: do we need to make this optional?
+            var value = parsed_code.code;
+            if (this.wechat.check_auth_code(value)) {
+                this.wechat.process_qr(value);
+                return true;
+            }
+            return PosModelSuper.prototype.scan_product.apply(this, arguments);
+        },
+        on_wechat: function(msg) {
+            this.add_qr_payment(
+                msg.order_ref,
+                msg.journal_id,
+                msg.total_fee / 100.0,
+                {
+                    micropay_id: msg.micropay_id,
+                },
+                // Auto validate payment
+                true
+            );
+        },
+        wechat_qr_payment: function(order, creg) {
+            /* Send request asynchronously */
+            var self = this;
+
+            var pos = this;
+            var terminal_ref = "POS/" + pos.config.name;
+            var pos_id = pos.config.id;
+
+            var lines = order.orderlines.map(function(r) {
+                return {
+                    // Always use 1 because quantity is taken into account in price field
+                    quantity: 1,
+                    quantity_full: r.get_quantity(),
+                    price: r.get_price_with_tax(),
+                    product_id: r.get_product().id,
+                };
+            });
+
+            // Send without repeating on failure
+            return rpc
+                .query({
+                    model: "wechat.order",
+                    method: "create_qr",
+                    kwargs: {
+                        lines: lines,
+                        order_ref: order.uid,
+                        pay_amount: order.get_due(),
+                        terminal_ref: terminal_ref,
+                        pos_id: pos_id,
+                        journal_id: creg.journal.id,
+                    },
+                })
+                .then(function(data) {
+                    if (data.code_url) {
+                        self.on_payment_qr(order, data.code_url);
+                    } else if (data.error) {
+                        self.show_warning(data.error);
+                    } else {
+                        self.show_warning("Unknown error");
+                    }
+                });
+        },
+    });
+
+    var OrderSuper = models.Order;
+    models.Order = models.Order.extend({
+        add_paymentline: function(cashregister) {
+            if (cashregister.journal.wechat === "native") {
+                this.pos.wechat_qr_payment(this, cashregister);
+                return;
+            }
+            return OrderSuper.prototype.add_paymentline.apply(this, arguments);
+        },
+    });
+
+    var PaymentlineSuper = models.Paymentline;
+    models.Paymentline = models.Paymentline.extend({
+        initialize: function(attributes, options) {
+            PaymentlineSuper.prototype.initialize.apply(this, arguments);
+            this.micropay_id = options.micropay_id;
+        },
+        // TODO: do we need to extend init_from_JSON too ?
+        export_as_JSON: function() {
+            var res = PaymentlineSuper.prototype.export_as_JSON.apply(this, arguments);
+            res.micropay_id = this.micropay_id;
+            return res;
         },
     });
 });
