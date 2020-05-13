bc44f0306958a17184c6e5140e323f67f87160a9
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-05-14 02:09:14 +0500

    :rainbow: pre-commit auto cleanup

--- pos_nfc/__manifest__.py
+++ pos_nfc/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2019 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
@@ -5,37 +6,26 @@
     "summary": """Use NFC readers to scan RFID as barcodes""",
     "category": "Point of Sale",
     # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=10.0",
-    "images": ['images/pos_nfs2.jpg'],
+    "images": ["images/pos_nfs2.jpg"],
     "version": "10.0.1.0.0",
     "application": False,
-
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "support": "apps@it-projects.info",
     "website": "https://it-projects.info/team/yelizariev",
     "license": "LGPL-3",
     # "price": 9.00,
     # "currency": "EUR",
-
-    "depends": [
-        "point_of_sale",
-    ],
+    "depends": ["point_of_sale",],
     "external_dependencies": {"python": [], "bin": []},
-    "data": [
-        "views/assets.xml",
-    ],
-    "demo": [
-    ],
-    "qweb": [
-    ],
-
+    "data": ["views/assets.xml",],
+    "demo": [],
+    "qweb": [],
     "post_load": None,
     "pre_init_hook": None,
     "post_init_hook": None,
     "uninstall_hook": None,
-
     "auto_install": False,
     "installable": True,
-
     # "demo_title": "Web-NFC support in POS",
     # "demo_addons": [
     # ],
