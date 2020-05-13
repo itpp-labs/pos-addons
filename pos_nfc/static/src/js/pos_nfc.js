535e00ff841483091bc16c977047fb945d8843e1
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-05-14 03:03:14 +0500

    :book: change copyright

--- pos_nfc/static/src/js/pos_nfc.js
+++ pos_nfc/static/src/js/pos_nfc.js
@@ -1,5 +1,5 @@
 /* Copyright 2019 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
-   License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html). */
+   License MIT (https://opensource.org/licenses/MIT). */
 odoo.define("pos_nfc", function(require) {
     "use strict";
     var BarcodeReader = require("point_of_sale.devices").BarcodeReader;
