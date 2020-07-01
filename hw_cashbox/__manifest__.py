d8e720a1906ee08819270f3fcee9cab26c2f3322
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-07-01 17:21:00 +0500

    :cherries: change support address

--- hw_cashbox/__manifest__.py
+++ hw_cashbox/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "11.0.1.0.0",
     "application": False,
     "author": "IT-Projects LLC, Artem Rafailov",
-    "support": "pos@it-projects.info",
+    "support": "apps@itpp.dev",
     "website": "https://it-projects.info/team/Ommo73",
     "license": "Other OSI approved licence",  # MIT
     # "price": 9.00,
