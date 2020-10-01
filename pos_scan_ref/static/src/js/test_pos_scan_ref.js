703f40aafbd3b6898c84252215aaf52947daed8b
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-10-01 15:11:55 +0500

    :shield: update tour after migration

--- pos_scan_ref/static/src/js/test_pos_scan_ref.js
+++ pos_scan_ref/static/src/js/test_pos_scan_ref.js
@@ -1,4 +1,5 @@
 /*  Copyright 2019 Artem Rafailov <https://it-projects.info/team/Ommo73>
+    Copyright 2020 Denis Mudarisov <https://github.com/trojikman>
     License MIT (https://opensource.org/licenses/MIT). */
 odoo.define("pos_scan_ref.tour", function(require) {
     "use strict";
@@ -9,16 +10,27 @@ odoo.define("pos_scan_ref.tour", function(require) {
     var _t = core._t;
 
     var steps = [
+        tour.STEPS.SHOW_APPS_MENU_ITEM,
         {
-            trigger:
-                '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"], .oe_menu_toggler[data-menu-xmlid="point_of_sale.menu_point_root"]',
-            content: _t("Ready to launch your point of sale? Click here."),
+            trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"]',
+            content: _t(
+                "Ready to launch your <b>point of sale</b>? <i>Click here</i>."
+            ),
+            position: "right",
+            edition: "community",
+        },
+        {
+            trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"]',
+            content: _t(
+                "Ready to launch your <b>point of sale</b>? <i>Click here</i>."
+            ),
             position: "bottom",
+            edition: "enterprise",
         },
         {
             trigger: ".o_pos_kanban button.oe_kanban_action_button",
             content: _t(
-                "Click to start the point of sale interface. It runs on tablets, laptops, or industrial hardware. Once the session launched, the system continues to run without an internet connection.</p>"
+                "<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>"
             ),
             position: "bottom",
         },
@@ -41,7 +53,7 @@ odoo.define("pos_scan_ref.tour", function(require) {
         },
         {
             content: "Check that the product was scanned successfully",
-            trigger: '.orderline.selected:contains("Boni Oranges")',
+            trigger: '.orderline.selected:contains("Cabinet with Doors")',
             position: "bottom",
         },
     ];
