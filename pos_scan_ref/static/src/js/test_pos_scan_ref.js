32e88509ffd7c3174a20d32bd7fd5ed06bd025fe
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-10-01 09:35:18 +0500

    :bomb:

--- pos_scan_ref/static/src/js/test_pos_scan_ref.js
+++ pos_scan_ref/static/src/js/test_pos_scan_ref.js
@@ -8,20 +8,46 @@ odoo.define("pos_scan_ref.tour", function(require) {
     var core = require("web.core");
     var _t = core._t;
 
+//tour.register('point_of_sale_tour', {
+//    url: "/web",
+//    rainbowMan: false,
+//}, [tour.STEPS.SHOW_APPS_MENU_ITEM, {
+//    trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"]',
+//    content: _t("Ready to launch your <b>point of sale</b>?"),
+//    width: 215,
+//    position: 'right',
+//    edition: 'community'
+//}, {
+//    trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"]',
+//    content: _t("Ready to launch your <b>point of sale</b>?"),
+//    width: 215,
+//    position: 'bottom',
+//    edition: 'enterprise'
+//}, {
+//    trigger: ".o_pos_kanban button.oe_kanban_action_button",
+//    content: _t("<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>"),
+//    position: "bottom"
+//}]);
+//
+//});
+
+
     var steps = [
-        {
-            trigger:
-                '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"], .oe_menu_toggler[data-menu-xmlid="point_of_sale.menu_point_root"]',
-            content: _t("Ready to launch your point of sale? Click here."),
-            position: "bottom",
-        },
-        {
+        tour.STEPS.SHOW_APPS_MENU_ITEM, {
+            trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"]',
+            content: _t("Ready to launch your <b>point of sale</b>? <i>Click here</i>."),
+            position: 'right',
+            edition: 'community'
+        }, {
+            trigger: '.o_app[data-menu-xmlid="point_of_sale.menu_point_root"]',
+            content: _t("Ready to launch your <b>point of sale</b>? <i>Click here</i>."),
+            position: 'bottom',
+            edition: 'enterprise'
+        }, {
             trigger: ".o_pos_kanban button.oe_kanban_action_button",
-            content: _t(
-                "Click to start the point of sale interface. It runs on tablets, laptops, or industrial hardware. Once the session launched, the system continues to run without an internet connection.</p>"
-            ),
-            position: "bottom",
-        },
+            content: _t("<p>Click to start the point of sale interface. It <b>runs on tablets</b>, laptops, or industrial hardware.</p><p>Once the session launched, the system continues to run without an internet connection.</p>"),
+            position: "bottom"
+        }
         {
             content: "Switch to table or make dummy action",
             trigger:
