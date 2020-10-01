3675d5444318b8298ed86557951db5e7dc408e63
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-10-01 15:11:55 +0500

    :shield: update tour after migration

--- pos_scan_ref/tests/test_pos_scan_ref.py
+++ pos_scan_ref/tests/test_pos_scan_ref.py
@@ -1,28 +1,19 @@
 # Copyright 2019 Artem Rafailov <https://it-projects.info/team/Ommo73/>
+# Copyright 2020 Denis Mudarisov <https://github.com/trojikman>
 # License MIT (https://opensource.org/licenses/MIT).
 
 import odoo.tests
-from odoo.api import Environment
 
 
-@odoo.tests.common.at_install(True)
-@odoo.tests.common.post_install(True)
+@odoo.tests.tagged("post_install", "-at_install")
 class TestUi(odoo.tests.HttpCase):
     def test_pos_scan_ref(self):
-        cr = self.registry.cursor()
-        env = Environment(cr, self.uid, {})
-        env["ir.module.module"].search(
-            [("name", "=", "pos_scan_ref")], limit=1
-        ).state = "installed"
-        env["product.template"].search([("name", "=", "Boni Oranges")], limit=1).write(
-            {"default_code": "1234567890333"}
-        )
-        cr.release()
-        # without a delay there might be problems on the steps whilst opening a POS
-        # caused by a not yet loaded button's action
-        self.phantom_js(
+        self.env["product.template"].search(
+            [("name", "=", "Cabinet with Doors")], limit=1
+        ).write({"default_code": "1234567890333"})
+        self.browser_js(
             "/web",
-            "odoo.__DEBUG__.services['web_tour.tour'].run('tour_pos_scan_ref', 1000)",
+            "odoo.__DEBUG__.services['web_tour.tour'].run('tour_pos_scan_ref')",
             "odoo.__DEBUG__.services['web_tour.tour'].tours.tour_pos_scan_ref.ready",
             login="admin",
             timeout=140,
