e165254cabb722586957e071fd38992d86970b8b
Mitchell Admin <itpp-bot@users.noreply.github.com>
2021-02-14 13:26:16 +0000

    :arrow_up::one::four: OCA/odoo-module-migrator
    close #1370
    
    > Made via .github/workflows/DINAR-PORT.yml

--- pos_orderline_absolute_discount/tests/test_default.py
+++ pos_orderline_absolute_discount/tests/test_default.py
@@ -5,7 +5,7 @@ import odoo.tests
 @odoo.tests.common.post_install(True)
 class TestUi(odoo.tests.HttpCase):
     def test_01_pos_is_loaded(self):
-        self.phantom_js(
+        self.browser_js(
             "/web",
             "odoo.__DEBUG__.services['web_tour.tour']"
             ".run('pos_orderline_absolute_discount_tour')",
