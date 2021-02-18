50b5fcb34a8a77158aad90c78263f2128bb1d358
Denis Mudarisov <denis.mudarisov@gmail.com>
2021-02-18 13:02:21 +0500

    :euro: make the module free
    if you need support or need us to fix a bug take a look at our subscription options https://www.patreon.com/itpp

--- pos_invoice_pay/__manifest__.py
+++ pos_invoice_pay/__manifest__.py
@@ -5,16 +5,13 @@
     "name": """POS: Pay SO & Invoices""",
     "summary": """Handle the payment process for Sale Orders/Invoices over Point of Sale""",
     "category": "Point of Sale",
-    # "live_test_url": "http://apps.it-projects.info/shop/product/pos-invoice-pay?version=11.0",
     "images": ["images/pos_invoice_pay_main.png"],
     "version": "11.0.1.2.4",
     "application": False,
     "author": "IT-Projects LLC, Artyom Losev",
-    "support": "apps@itpp.dev",
-    "website": "https://it-projects.info/team/ArtyomLosev",
+    "support": "help@itpp.dev",
+    "website": "https://apps.odoo.com/apps/modules/11.0/pos_invoice_pay",
     "license": "Other OSI approved licence",  # MIT
-    "price": 89.00,
-    "currency": "EUR",
     "depends": ["base_automation", "sale_management", "pos_longpolling"],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
