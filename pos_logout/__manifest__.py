bb4bcef30a25fe12abb6dffd3de3ae5e1d3cb1b6
Denis Mudarisov <denis.mudarisov@gmail.com>
2021-03-01 10:46:06 +0500

    :sparkles: we don't need pos_pin dependency anymore due to existing functionality

--- pos_logout/__manifest__.py
+++ pos_logout/__manifest__.py
@@ -2,6 +2,7 @@
 # Copyright 2017-2018 Ilmir Karamov <https://it-projects.info/team/ilmir-k>
 # Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # Copyright 2020 Almas Giniatullin <https://it-projects.info/team/almas50>
+# Copyright 2021 Denis Mudarisov <https://github.com/trojikman>
 # License MIT (https://opensource.org/licenses/MIT).
 {
     "name": """Lock POS Screen""",
@@ -9,7 +10,7 @@
     "category": "Point of Sale",
     # "live_test_url": "http://apps.it-projects.info/shop/product/pos-logout?version=13.0",
     "images": ["images/pos_logout_main.png"],
-    "version": "13.0.1.0.0",
+    "version": "13.0.2.0.0",
     "application": False,
     "author": "IT-Projects LLC, Dinar Gabbasov",
     "support": "apps@itpp.dev",
@@ -17,7 +18,7 @@
     "license": "Other OSI approved licence",  # MIT
     "price": 20.00,
     "currency": "EUR",
-    "depends": ["pos_pin"],
+    "depends": ["point_of_sale"],
     "external_dependencies": {"python": [], "bin": []},
     "data": ["views/pos_logout.xml"],
     "qweb": ["static/src/xml/pos.xml"],
