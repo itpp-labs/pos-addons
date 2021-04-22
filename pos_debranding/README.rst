b2e2d31689980536c33b9d093bf645094d38d0ed
Lucas Soto <lucaslsoto95@gmail.com>
2021-04-22 12:37:38 -0300

    [13.0] pos_debranding

---
+++ pos_debranding/README.rst
@@ -0,0 +1,14 @@
+.. image:: https://itpp.dev/images/infinity-readme.png
+   :alt: Tested and maintained by IT Projects Labs
+   :target: https://itpp.dev
+
+================
+ POS debranding
+================
+
+Removes references to odoo.com:
+
+1. Deletes odoo logo
+2. Replaces POS title
+
+Tested on `Odoo 9.0 <https://github.com/odoo/odoo/commit/04c6ee54d86013bc2995778f62074115c1bd9ed3>`_
---
+++ pos_debranding/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "POS debranding",
+    "version": "13.0.1.0.0",
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "license": "Other OSI approved licence",  # MIT
+    "category": "Debranding",
+    "support": "apps@itpp.dev",
+    "website": "https://www.odoo.com/apps/modules/13.0/pos_debranding/",
+    "depends": ["point_of_sale"],
+    # 'price': 30.00,
+    # 'currency': 'EUR',
+    "data": ["views.xml", "template.xml"],
+    "qweb": ["static/src/xml/pos_debranding.xml"],
+    "installable": True,
+}
