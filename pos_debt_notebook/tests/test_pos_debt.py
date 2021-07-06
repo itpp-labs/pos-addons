253ebff4406db3f49344cccf45ea39664830d581
Ivan Yelizariev <iel@odoo.com>
2021-07-06 18:36:48 +0200

    Revert ":arrow_up::x: pos_debt_notebook is ported and available by the following link https://apps.odoo.com/apps/modules/13.0/pos_debt_notebook"
    This reverts commit c77ebc1e9c27a2df45a027c0abaedc7ea098c8e7.

---
+++ pos_debt_notebook/tests/test_pos_debt.py
@@ -0,0 +1,25 @@
+import odoo.tests
+
+
+@odoo.tests.common.at_install(True)
+@odoo.tests.common.post_install(True)
+class TestUi(odoo.tests.HttpCase):
+    def test_pos_debt(self):
+        # needed because tests are run before the module is marked as
+        # installed. In js web will only load qweb coming from modules
+        # that are returned by the backend in module_boot. Without
+        # this you end up with js, css but no qweb.
+        env = self.env
+        env["ir.module.module"].search(
+            [("name", "=", "pos_debt_notebook")], limit=1
+        ).state = "installed"
+
+        # without a delay there might be problems on the steps whilst opening a POS
+        # caused by a not yet loaded button's action
+        self.phantom_js(
+            "/web",
+            "odoo.__DEBUG__.services['web_tour.tour'].run('tour_pos_debt_notebook', 1000)",
+            "odoo.__DEBUG__.services['web_tour.tour'].tours.tour_pos_debt_notebook.ready",
+            login="admin",
+            timeout=1000,
+        )
