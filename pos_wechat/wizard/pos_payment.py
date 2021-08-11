cabcc643bb4161f42eb2a02010a10b742700ddfc
Mitchell Admin <itpp-bot@users.noreply.github.com>
2021-08-11 11:01:44 +0000

    :tada::one::three: pos_wechat previous commits history: https://github.com/itpp-labs/pos-addons/commits/13.0/pos_wechat
    > Made via .github/workflows/DINAR-PORT.yml

---
+++ pos_wechat/wizard/pos_payment.py
@@ -0,0 +1,39 @@
+# Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# License MIT (https://opensource.org/licenses/MIT).
+from odoo import api, fields, models
+
+
+class PosMakePayment(models.TransientModel):
+    _inherit = "pos.make.payment"
+
+    journal_wechat = fields.Selection(related="journal_id.wechat")
+    wechat_order_id = fields.Many2one("wechat.order", string="WeChat Order to refund")
+    micropay_id = fields.Many2one("wechat.micropay", string="Micropay to refund")
+
+    def check(self):
+        res = super(PosMakePayment, self).check()
+        record = self.wechat_order_id or self.micropay_id
+        if record and self.amount < 0:
+            refund_fee = int(-100 * self.amount)
+            refund_vals = {
+                "order_id": self.wechat_order_id.id,
+                "micropay_id": self.micropay_id.id,
+                "total_fee": record.total_fee,
+                "refund_fee": refund_fee,
+                "journal_id": self.journal_id.id,
+            }
+            refund = self.env["wechat.refund"].create(refund_vals)
+            refund.action_confirm()
+        return res
+
+    @api.onchange("order_ref", "journal_wechat")
+    def update_wechat_order(self):
+        domain = [("order_ref", "=", self.order_ref)]
+        if self.journal_wechat == "micropay":
+            record = self.env["wechat.micropay"].search(domain)[:1]
+            self.wechat_order_id = False
+            self.micropay_id = record
+        elif self.journal_wechat == "native":
+            record = self.env["wechat.order"].search(domain)[:1]
+            self.wechat_order_id = record
+            self.micropay_id = False
