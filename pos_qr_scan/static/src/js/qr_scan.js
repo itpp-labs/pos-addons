8f5a7a7ee3e009cbb459188d70d51507e77880db
Mitchell Admin <itpp-bot@users.noreply.github.com>
2021-08-11 09:17:30 +0000

    :rainbow: pre-commit > Made via .github/workflows/DINAR-PORT.yml

--- pos_qr_scan/static/src/js/qr_scan.js
+++ pos_qr_scan/static/src/js/qr_scan.js
@@ -3,7 +3,7 @@
    License MIT (https://opensource.org/licenses/MIT). */
 /* global posmodel, qrcode, success, error, gCtx*/
 /* eslint no-alert: "off"*/
-odoo.define("pos_qr_scan", function(require) {
+odoo.define("pos_qr_scan", function (require) {
     "use strict";
 
     var core = require("web.core");
@@ -14,7 +14,7 @@ odoo.define("pos_qr_scan", function(require) {
 
     var QrButton = screens.ActionButtonWidget.extend({
         template: "QrButton",
-        button_click: function() {
+        button_click: function () {
             this.gui.show_popup("qr_scan", {
                 title: "QR Scanning",
                 value: false,
@@ -29,22 +29,22 @@ odoo.define("pos_qr_scan", function(require) {
 
     var QrScanPopupWidget = PopupWidget.extend({
         template: "QrScanPopupWidget",
-        show: function(options) {
+        show: function (options) {
             this.gUM = false;
             this._super(options);
             this.generate_qr_scanner();
         },
-        click_cancel: function() {
+        click_cancel: function () {
             this.stop_camera();
             this._super(arguments);
         },
-        stop_camera: function(camera) {
+        stop_camera: function (camera) {
             this.cam_is_on = false;
             if (this.stream) {
                 this.stream.getTracks()[0].stop();
             }
         },
-        add_button: function(content) {
+        add_button: function (content) {
             var button = document.createElement("div");
             button.className = "button qr-content";
             button.innerHTML = content.name;
@@ -52,33 +52,33 @@ odoo.define("pos_qr_scan", function(require) {
             button = $(".transparent_sidebar > .body").append(button);
             return button;
         },
-        add_button_click: function(e) {
+        add_button_click: function (e) {
             var button = document.createElement("div");
             var active_id = e.target.getAttribute("camera-id");
             this.start_webcam({deviceId: {exact: active_id}});
             this.pos.db.save("active_camera_id", active_id);
             return button;
         },
-        get_camera_by_id: function(id) {
-            return _.find(this.video_devices, function(cam) {
+        get_camera_by_id: function (id) {
+            return _.find(this.video_devices, function (cam) {
                 return cam.deviceId === id;
             });
         },
-        generate_qr_scanner: function() {
+        generate_qr_scanner: function () {
             var options = false;
             var self = this;
             this.video_element = document.getElementById("preview");
-            $(this.video_element).on("click", function() {
+            $(this.video_element).on("click", function () {
                 self.click_cancel();
             });
             if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
                 this.capture_timeout = 700;
                 try {
-                    navigator.mediaDevices.enumerateDevices().then(function(devices) {
-                        self.video_devices = _.filter(devices, function(d) {
+                    navigator.mediaDevices.enumerateDevices().then(function (devices) {
+                        self.video_devices = _.filter(devices, function (d) {
                             return d.kind === "videoinput";
                         });
-                        _.each(self.video_devices, function(device) {
+                        _.each(self.video_devices, function (device) {
                             options = options || {deviceId: {exact: device.deviceId}};
                             if (device.label.toLowerCase().search("back") > -1) {
                                 options = {
@@ -89,7 +89,7 @@ odoo.define("pos_qr_scan", function(require) {
                             }
                             self.add_button({name: device.label, id: device.deviceId})
                                 .off()
-                                .on("click", function(e) {
+                                .on("click", function (e) {
                                     self.add_button_click(e);
                                 });
                         });
@@ -114,7 +114,7 @@ odoo.define("pos_qr_scan", function(require) {
             }
         },
 
-        read: function(result) {
+        read: function (result) {
             // Trigger event on scanning and close camera window
             if (this.pos.debug) {
                 console.log("QR scanned", result);
@@ -123,20 +123,20 @@ odoo.define("pos_qr_scan", function(require) {
             posmodel.gui.popup_instances.qr_scan.click_cancel();
         },
 
-        start_webcam: function(options) {
+        start_webcam: function (options) {
             var self = this;
             this.initCanvas(800, 600);
-            qrcode.callback = function(value) {
+            qrcode.callback = function (value) {
                 self.read(value);
             };
             if (navigator.mediaDevices.getUserMedia) {
                 navigator.mediaDevices
                     .getUserMedia({video: options, audio: false})
-                    .then(function(stream) {
+                    .then(function (stream) {
                         self.stream = stream;
                         self.success(stream);
                     })
-                    .catch(function(error) {
+                    .catch(function (error) {
                         self.gui.show_popup("error-traceback", {
                             title: error.name + " " + error.code,
                             body: error.message,
@@ -154,17 +154,17 @@ odoo.define("pos_qr_scan", function(require) {
             }
 
             this.cam_is_on = true;
-            setTimeout(function() {
+            setTimeout(function () {
                 self.captureToCanvas();
             }, this.capture_timeout);
         },
 
-        success: function(stream) {
+        success: function (stream) {
             this.video_element.srcObject = stream;
             this.video_element.play();
             this.gUM = true;
         },
-        captureToCanvas: function() {
+        captureToCanvas: function () {
             if (!this.cam_is_on) {
                 return;
             }
@@ -176,19 +176,19 @@ odoo.define("pos_qr_scan", function(require) {
                         qrcode.decode();
                     } catch (e) {
                         console.log(e);
-                        setTimeout(function() {
+                        setTimeout(function () {
                             self.captureToCanvas();
                         }, this.capture_timeout);
                     }
                 } catch (e) {
                     console.log(e);
-                    setTimeout(function() {
+                    setTimeout(function () {
                         self.captureToCanvas();
                     }, this.capture_timeout);
                 }
             }
         },
-        initCanvas: function(w, h) {
+        initCanvas: function (w, h) {
             var gCanvas = document.getElementById("qr-canvas");
             gCanvas.style.width = w + "px";
             gCanvas.style.height = h + "px";
@@ -204,7 +204,7 @@ odoo.define("pos_qr_scan", function(require) {
     // Add some helpers:
     // * No need to show Cashregister that is used by scanning QR customer's code
     models.PosModel = models.PosModel.extend({
-        hide_cashregister: function(journal_filter) {
+        hide_cashregister: function (journal_filter) {
             var self = this;
             var journal = _.filter(this.journals, journal_filter);
             if (journal.length) {
@@ -220,7 +220,7 @@ odoo.define("pos_qr_scan", function(require) {
             } else {
                 return false;
             }
-            self.cashregisters = _.filter(self.cashregisters, function(r) {
+            self.cashregisters = _.filter(self.cashregisters, function (r) {
                 if (r.journal_id[0] === journal.id) {
                     self.hidden_cashregisters.push(r);
                     return false;
