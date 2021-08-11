b81e84d27321d0c268f8a0b8c7c0eeff69d91db8
Mitchell Admin <itpp-bot@users.noreply.github.com>
2021-08-11 11:17:07 +0000

    :tada::one::three: qr_payments previous commits history: https://github.com/itpp-labs/pos-addons/commits/13.0/qr_payments
    > Made via .github/workflows/DINAR-PORT.yml

---
+++ qr_payments/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# License MIT (https://opensource.org/licenses/MIT).
+{
+    "name": """QR-based payments""",
+    "summary": """Technical module to support qr-based payments like Alipay, WeChat""",
+    "category": "Hidden",
+    "images": ["images/QR-based payments.jpg"],
+    "version": "13.0.1.0.1",
+    "application": False,
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "help@itpp.dev",
+    "website": "https://github.com/itpp-labs/pos-addons#readme",
+    "license": "Other OSI approved licence",  # MIT
+    "depends": [],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [],
+    "demo": [],
+    "qweb": [],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+    "auto_install": False,
+    "installable": False,
+}
---
+++ qr_payments/tools/__init__.py
@@ -0,0 +1,2 @@
+# License MIT (https://opensource.org/licenses/MIT).
+from .async_call import odoo_async_call
