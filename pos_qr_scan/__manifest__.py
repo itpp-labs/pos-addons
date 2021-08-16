4d8653294d38c46175d79e879e5976fd492ac577
Mitchell Admin <itpp-bot@users.noreply.github.com>
2021-08-16 07:29:22 +0000

    :tada::one::three: pos_qr_scan previous commits history: https://github.com/itpp-labs/pos-addons/commits/13.0/pos_qr_scan
    > Made via .github/workflows/DINAR-PORT.yml

---
+++ pos_qr_scan/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": """QR Code Scanning in POS""",
+    "summary": """Scans QR codes via device's camera""",
+    "category": "Point of Sale",
+    "images": ["images/main.png"],
+    "version": "13.0.1.0.2",
+    "application": False,
+    "author": "IT-Projects LLC, KolushovAlexandr",
+    "support": "help@itpp.dev",
+    "website": "https://github.com/itpp-labs/pos-addons#readme",
+    "license": "Other OSI approved licence",  # MIT
+    "depends": ["pos_qr_payments"],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": ["views/assets.xml"],
+    "qweb": ["static/src/xml/templates.xml"],
+    "auto_install": False,
+    "installable": False,
+}
