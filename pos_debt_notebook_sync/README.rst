c1b2fa09f6a727122fad30b0204a06616694f2ba
Mitchell Admin <itpp-bot@users.noreply.github.com>
2022-09-14 09:51:29 +0000

    :tada::one::three: pos_debt_notebook_sync previous commits history: https://github.com/itpp-labs/pos-addons/commits/13.0/pos_debt_notebook_sync > Made via .github/workflows/DINAR-PORT.yml

---
+++ pos_debt_notebook_sync/README.rst
@@ -0,0 +1,30 @@
+.. image:: https://itpp.dev/images/infinity-readme.png
+   :alt: Tested and maintained by IT Projects Labs
+   :target: https://itpp.dev
+
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg
+   :target: https://opensource.org/licenses/MIT
+   :alt: License: MIT
+
+========================
+ Internal Credit System
+========================
+
+The module allows to organize cash-free system for set of shops with a single payment point. E.g. at festivals, amusements parks, etc.
+
+Questions?
+==========
+
+To get an assistance on this module contact us by email :arrow_right: help@itpp.dev
+
+Contributors
+============
+* `Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>`__
+* `Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>`__
+
+===================
+
+Odoo Apps Store: https://apps.odoo.com/apps/modules/12.0/pos_debt_notebook_sync/
+
+
+Tested on `Odoo 13.0 <https://github.com/odoo/odoo/commit/2d46a08a989f4c7e005ee2ead7c342c72019e63f>`_
---
+++ pos_debt_notebook_sync/models/pos_config.py
@@ -0,0 +1,37 @@
+# Copyright 2017 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# License MIT (https://opensource.org/licenses/MIT).
+
+from odoo import api, models
+
+CHANNEL = "pos_debt_notebook_sync"
+
+
+class PosConfig(models.Model):
+    _inherit = "pos.config"
+
+    # ir.actions.server methods:
+    @api.model
+    def notify_debt_updates(self):
+        model = self.env.context["active_model"]
+        ids = self.env.context["active_ids"]
+        records = self.env[model].sudo().browse(ids)
+
+        partners = None
+        if model == "account.bank.statement.line":
+            partners = records.mapped(lambda r: r.pos_statement_id.partner_id)
+        elif model == "pos.order":
+            partners = records.filtered(
+                lambda r: r.state in ["paid", "done"]
+                and any(line.product_id.credit_product for line in r.lines)
+            ).mapped(lambda r: r.partner_id)
+        elif model == "account.invoice":
+            partners = records.filtered(
+                lambda r: r.state in ["paid"]
+                and any(line.product_id.credit_product for line in r.invoice_line_ids)
+            ).mapped(lambda r: r.partner_id)
+        elif model == "pos.credit.update":
+            partners = records.mapped(lambda r: r.partner_id)
+
+        if partners:
+            message = {"updated_partners": partners.ids}
+            self.search([])._send_to_channel(CHANNEL, message)
